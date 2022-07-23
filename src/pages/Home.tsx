fc529bbe6672cfbe0f98d9a2708462c744c5aa89
John Doe <johndoe@example.com>
2022-07-23 12:49:50 +0800

    fix:Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,14 @@
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import { txt } from '../../README.md';
+
 export default function About() {
   return (
     <>
-      <h1>Home</h1>
+      <ReactMarkdown
+        children={txt}
+        remarkPlugins={[remarkGfm]}
+      />
     </>
-  )
+  );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+function txt(regexp: RegExp) {
+  return {
+    name: 'txt',
+    transform(src: string, id: string) {
+      if (regexp.test(id)) {
+        return {
+          code: `export const txt = \`${src.replace(/`/g, '\\`')}\`;`,
+          map: null // provide source map if available
+        }
+      }
+    }
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), txt(/\.md$/)]
 })
