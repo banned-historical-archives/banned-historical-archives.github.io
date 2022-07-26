8fd06a44f269dfed9b3092a95e7ca3b033e4b49b
John Doe <johndoe@example.com>
2022-07-26 12:59:48 +0800

    fix:service

--- src/service.ts
+++ src/service.ts
@@ -5,11 +5,21 @@ import type Content from '../backend/entity/content';
 import type Page from '../backend/entity/page';
 
 export async function get_article(articleId: string) {
-  const res = await axios.get<Article>(`/article/${articleId}.json`);
+  const res = await axios.get<Article>(`/article/${articleId}.json`, {
+    headers: {
+      accept: 'application/json',
+    },
+    data: {},
+  });
   return res.data;
 }
 
 export async function get_articles() {
-  const res = await axios.post<Article[]>('/articles.json');
+  const res = await axios.post<Article[]>('/articles.json', {
+    headers: {
+      accept: 'application/json',
+    },
+    data: {},
+  });
   return res.data;
 }
