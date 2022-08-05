88365e5eaa730a60dc47106822c41a09f8717b41
John Doe <johndoe@example.com>
2022-08-05 13:31:48 +0800

    fix

--- pages/articles/[id].tsx
+++ pages/articles/[id].tsx
@@ -725,7 +725,7 @@ export default function ArticleViewer({
           ) : publication.type === 'img' ? (
             publication.files
               .split(',')
-              .filter((i, idx) => idx + 1 >= page.start && idx + 1 <= page.end)
+              .filter((i, idx) => idx + 1>= page.start && idx + 1 <= page.end)
               .map((f) => <img key={f} src={f} />)
           ) : (
             <>未知类型</>
