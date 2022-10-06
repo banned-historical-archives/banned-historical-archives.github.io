73d518752bc7161428ffaebcef124e4d911cb1bc
PirateBook1 <news_porter@protonmail.com>
2022-10-06 18:17:39 +0800

    fix title of duoren9.ts

--- backend/parser/duoren9.ts
+++ backend/parser/duoren9.ts
@@ -65,7 +65,7 @@ export async function parse(
       title: title2,
       parts: [
         {
-          text: title,
+          text: title2,
           type: ContentType.title,
         },
         ...`
