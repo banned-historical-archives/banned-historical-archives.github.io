cfe0d6ef80b88bb6d1d442e99c5891c14597269f
Your Name <you@example.com>
2022-10-17 18:03:32 +0800

    debug

--- backend/debug.ts
+++ backend/debug.ts
@@ -15,6 +15,7 @@ import { basename } from 'node:path';
   const book = books.find(i => i.entity.id === '78256f65-d15b-4800-976e-d4a9e6e7a160')!;
   const res = await book.parser(book.path, book.parser_option);
   console.log(res);
+  console.log(res.map(i => get_article_id(i)));
 
   debugger;
 })();
