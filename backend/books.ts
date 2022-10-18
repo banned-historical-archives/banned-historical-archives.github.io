3eef40d4d28386d43457c7729ebcf98e35cef2bb
PirateBook1 <PirateBook1@users.noreply.github.com>
2022-10-18 14:33:41 +0800

    [create-pull-request] automated change

--- backend/books.ts
+++ backend/books.ts
@@ -66,6 +66,34 @@ const patch_dir = join(normalize(__dirname), '../patch/articles');
 const books: Book[] = [
   {
     entity: {
+      id: 'febb7416-870c-4ca2-85c1-00de96d8a9d0',
+      name: '毛远新谈话记录',
+      internal: true,
+      official: true,
+      type: 'img',
+      author: '',
+      files: new Array(4)
+        .fill(0)
+        .map(
+          (i, idx) =>
+            `https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/febb7416-870c-4ca2-85c1-00de96d8a9d0/${
+              idx + 1
+            }.png`,
+        )
+        .join(','),
+    },
+    parser_option: {
+      page_limits: [],
+      ext: 'png',
+      articles: [{"title":"毛远新谈话记录","authors":["毛远新"],"page_start":2,"page_end":4,"dates":[{"year":1976,"month":4,"day":12}]}],
+      ocr: undefined,
+      ocr_exceptions: {},
+    },
+    parser: automation.parse,
+    path: join(normalize(__dirname), '../public/books/archives1/febb7416-870c-4ca2-85c1-00de96d8a9d0'),
+  },
+  {
+    entity: {
       id: '680acdfe-228a-4f53-86ec-bfbc17aff79d',
       name: '毛远新在鞍山市党员干部会议上的讲话涉及宋江的部分',
       internal: true,
