155eadd017723d8a98cb87259a253ca6bb307e07
PirateBook1 <PirateBook1@users.noreply.github.com>
2022-10-21 14:42:03 +0800

    [create-pull-request] automated change

--- backend/books.ts
+++ backend/books.ts
@@ -66,6 +66,26 @@ const patch_dir = join(normalize(__dirname), '../patch/articles');
 const books: Book[] = [
   {
     entity: {
+      id: 'd7255791-fc0e-4d11-9391-b5a8c1d3cf8b',
+      name: '社会主义政治经济学 （未定稿第二版讨论稿）',
+      internal: true,
+      official: true,
+      type: 'pdf',
+      author: '',
+      files: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives3/main/d7255791-fc0e-4d11-9391-b5a8c1d3cf8b.pdf',
+    },
+    parser_option: {
+      page_limits: [],
+      ext: 'pdf',
+      articles: [{"title":"序言","authors":["《社会主义政治经济学》编写组"],"page_start":11,"page_end":19,"dates":[{"year":1976}]},{"title":"第一章 社会主义所有制","authors":["《社会主义政治经济学》编写组"],"page_start":25,"page_end":74,"dates":[{"year":1976}]},{"title":"第二章 社会主义生产中的相互关系","authors":["《社会主义政治经济学》编写组"],"page_start":75,"page_end":112,"dates":[{"year":1976}]},{"title":"第三章 社会主义生产的性质和目的","authors":["《社会主义政治经济学》编写组"],"page_start":115,"page_end":155,"dates":[{"year":1976}]},{"title":"第四章 社会主义生产的性质和目的","authors":["《社会主义政治经济学》编写组"],"page_start":159,"page_end":201,"dates":[{"year":1976}]},{"title":"第五章 社会主义农业和工业","authors":["《社会主义政治经济学》编写组"],"page_start":205,"page_end":252,"dates":[{"year":1976}]},{"title":"第六章 社会主义制度下的节约","authors":["《社会主义政治经济学》编写组"],"page_start":253,"page_end":275,"dates":[{"year":1976}]},{"title":"第七章 社会主义经济核算制","authors":["《社会主义政治经济学》编写组"],"page_start":281,"page_end":309,"dates":[{"year":1976}]},{"title":"第八章 社会主义社会的商品交换","authors":["《社会主义政治经济学》编写组"],"page_start":313,"page_end":360,"dates":[{"year":1976}]},{"title":"第九章 社会主义社会的货币和货币流通","authors":["《社会主义政治经济学》编写组"],"page_start":361,"page_end":387,"dates":[{"year":1976}]},{"title":"第十章 社会主义社会的分配","authors":["《社会主义政治经济学》编写组"],"page_start":393,"page_end":437,"dates":[{"year":1976}]},{"title":"第十一章 社会主义积累和扩大再生产","authors":["《社会主义政治经济学》编写组"],"page_start":441,"page_end":474,"dates":[{"year":1976}]},{"title":"第十一章 社会主义积累和扩大再生产","authors":["《社会主义政治经济学》编写组"],"page_start":441,"page_end":474,"dates":[{"year":1976}]},{"title":"第十二章 社会主义积累和扩大再生产","authors":["《社会主义政治经济学》编写组"],"page_start":475,"page_end":509,"dates":[{"year":1976}]},{"title":"第十三章 社会主义生产关系和阶级关系","authors":["《社会主义政治经济学》编写组"],"page_start":515,"page_end":524,"dates":[{"year":1976}]},{"title":"第十四章 无产阶级专政下的继续革命","authors":["《社会主义政治经济学》编写组"],"page_start":525,"page_end":534,"dates":[{"year":1976}]},{"title":"第十五章 共产主义是社会主义发展的必然趋势","authors":["《社会主义政治经济学》编写组"],"page_start":535,"page_end":551,"dates":[{"year":1976}]}],
+      ocr: undefined,
+      ocr_exceptions: {},
+    },
+    parser: automation.parse,
+    path: join(normalize(__dirname), '../public/books/archives3/d7255791-fc0e-4d11-9391-b5a8c1d3cf8b.pdf'),
+  },
+  {
+    entity: {
       id: '7c69c3ac-22a8-483f-b7ba-33fb0081f388',
       name: '毛远新在省工农兵干部学习班的几次讲话（摘编）',
       internal: true,
