86c6e88a6916150234c566df6b1827fa57f76a48
banned-historical-archives <banned-historical-archives@users.noreply.github.com>
2022-10-21 20:16:41 +0800

    [create-pull-request] automated change

--- backend/books.ts
+++ backend/books.ts
@@ -66,6 +66,26 @@ const patch_dir = join(normalize(__dirname), '../patch/articles');
 const books: Book[] = [
   {
     entity: {
+      id: 'ff69a7c6-9559-4b80-98fa-cfbd3c072a56',
+      name: '红旗一九六六年第六期',
+      internal: true,
+      official: true,
+      type: 'pdf',
+      author: '',
+      files: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives2/main/ff69a7c6-9559-4b80-98fa-cfbd3c072a56.pdf',
+    },
+    parser_option: {
+      page_limits: [],
+      ext: 'pdf',
+      articles: [{"title":"高举毛泽东思想伟大红旗积极参加社会主义文化大革命","authors":["《解放军报》社论"],"page_start":3,"page_end":11,"dates":[{"year":1966}]},{"title":"工农兵群众批判吴晗反党反社会主义的政治立场和学术观点","authors":[],"page_start":12,"page_end":21,"ocr":{"vsplit":0.35},"dates":[{"year":1966}]},{"title":"工农兵群众参加学术批判是划时代的大事","authors":["红旗杂志评论员"],"page_start":22,"page_end":24,"dates":[{"year":1966}]},{"title":"评吴晗的《投枪集》","authors":["史绍宾"],"page_start":25,"page_end":35,"ocr":{"vsplit":0.35},"dates":[{"year":1966}]}],
+      ocr: {"auto_vsplit":false,"vsplit":0,"content_thresholds":[0,0.1,0,0.1]},
+      ocr_exceptions: {"10":{"vsplit":0.35},"11":{"vsplit":0.35}},
+    },
+    parser: automation.parse,
+    path: join(normalize(__dirname), '../public/books/archives2/ff69a7c6-9559-4b80-98fa-cfbd3c072a56.pdf'),
+  },
+  {
+    entity: {
       id: 'b4980847-3787-4b09-b95f-d4bc32199051',
       name: '红旗一九六六年第五期',
       internal: true,
