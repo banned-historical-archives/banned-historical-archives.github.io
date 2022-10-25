3cfa6652a398d4a31a2d82b1075d2297a72d9df8
PirateBook1 <PirateBook1@users.noreply.github.com>
2022-10-25 11:54:02 +0800

    [create-pull-request] automated change

--- backend/books.ts
+++ backend/books.ts
@@ -66,6 +66,34 @@ const patch_dir = join(normalize(__dirname), '../patch/articles');
 const books: Book[] = [
   {
     entity: {
+      id: 'c02feeb6-58b4-44b9-9965-47de5504e4b7',
+      name: '毛远新在批邓、反击右倾翻案风斗争中的一些言论（摘编）',
+      internal: true,
+      official: true,
+      type: 'img',
+      author: '',
+      files: new Array(10)
+        .fill(0)
+        .map(
+          (i, idx) =>
+            `https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/c02feeb6-58b4-44b9-9965-47de5504e4b7/${
+              idx + 1
+            }.png`,
+        )
+        .join(','),
+    },
+    parser_option: {
+      page_limits: [],
+      ext: 'png',
+      articles: [{"title":"（一）毛远新在反击右倾翻案风开始时的讲话（摘录）","authors":["毛远新"],"page_start":2,"page_end":5,"ocr_exceptions":{"2":{"content_thresholds":[0,0,0.5,0]},"5":{"content_thresholds":[0,0,0,0.5]}},"dates":[{"year":1975,"month":11,"day":29},{"year":1975,"month":12,"day":1},{"year":1975,"month":12,"day":2}]},{"title":"毛远新的谈话记录","alias":"（二）毛远新一九七六年一月一日的谈话（摘录）","authors":["毛远新"],"page_start":5,"page_end":7,"ocr_exceptions":{"7":{"content_thresholds":[0,0,0,0.5]}},"dates":[{"year":1976,"month":1,"day":1}]},{"title":"（三）毛远新一九七六年二月八日的谈话（摘录）","authors":["毛远新"],"page_start":7,"page_end":7,"dates":[{"year":1976,"month":2,"day":8}]},{"title":"毛远新对当前工作的意见","alias":"（四）毛远新一九七六年四月十九日谈对当前工作的意见","authors":["毛远新"],"page_start":7,"page_end":8,"ocr_exceptions":{"7":{"content_thresholds":[0,0,0.5,0]},"8":{"content_thresholds":[0,0,0,0.5]}},"dates":[{"year":1976,"month":4,"day":19}]},{"title":"毛远新谈话记录","alias":"（五）毛远新一九七六年四月二十二日的谈话（摘录）","authors":["毛远新"],"page_start":8,"page_end":9,"ocr_exceptions":{"8":{"content_thresholds":[0,0,0.5,0]},"9":{"content_thresholds":[0,0,0,0.5]}},"dates":[{"year":1976,"month":4,"day":22}]},{"title":"毛远新的两次谈话记录","alias":"（六）毛远新一九七六年六月二日、五日的两次谈话（摘录）","authors":["毛远新"],"page_start":9,"page_end":10,"dates":[{"year":1976,"month":6,"day":2},{"year":1976,"month":6,"day":5}]}],
+      ocr: undefined,
+      ocr_exceptions: {},
+    },
+    parser: automation.parse,
+    path: join(normalize(__dirname), '../public/books/archives1/c02feeb6-58b4-44b9-9965-47de5504e4b7'),
+  },
+  {
+    entity: {
       id: '9000310e-2f60-4711-a81d-a8fd520f6db7',
       name: '怎样理解资产阶级在党内？——宗明兰同志在辽宁省理论讨论会上的总结发言（记录整理）',
       internal: true,
