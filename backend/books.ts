92f0236a2b37243761e3647c94d14b93959dbe30
PirateBook1 <PirateBook1@users.noreply.github.com>
2022-10-27 11:47:30 +0800

    [create-pull-request] automated change

--- backend/books.ts
+++ backend/books.ts
@@ -59,6 +59,34 @@ const patch_dir = join(normalize(__dirname), '../patch/articles');
 const books: Book[] = [
   {
     entity: {
+      id: '96cdc4a4-0b09-4d57-8277-6dbcbcf6e4e0',
+      name: '四人帮反党罪行选编（二）',
+      internal: true,
+      official: true,
+      type: 'img',
+      author: '',
+      files: new Array(18)
+        .fill(0)
+        .map(
+          (i, idx) =>
+            `https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/96cdc4a4-0b09-4d57-8277-6dbcbcf6e4e0/${
+              idx + 1
+            }.png`,
+        )
+        .join(','),
+    },
+    parser_option: {
+      page_limits: [],
+      ext: 'png',
+      articles: [{"title":"江青九月二十七日在清华农村分校散布的反党言论摘录","authors":["江青"],"page_start":4,"page_end":6,"dates":[{"year":1976,"month":9,"day":27}]},{"title":"江青九月二十八日下午在清华农村分校花生地里散布的反党言论摘录","authors":["江青"],"page_start":7,"page_end":11,"dates":[{"year":1976,"month":9,"day":28}]},{"title":"江青九月二十九日在清华工程物理系散布的反党言论摘录","authors":["江青"],"page_start":12,"page_end":18,"dates":[{"year":1976,"month":9,"day":29}]}],
+      ocr: {"content_thresholds":[0,0.08,0,0]},
+      ocr_exceptions: {},
+    },
+    parser: automation.parse,
+    path: join(normalize(__dirname), '../public/books/archives1/96cdc4a4-0b09-4d57-8277-6dbcbcf6e4e0'),
+  },
+  {
+    entity: {
       id: 'c748a5f5-0b62-433b-b425-a3295b905902',
       name: '四人帮反党罪行材料选编（一）',
       internal: true,
