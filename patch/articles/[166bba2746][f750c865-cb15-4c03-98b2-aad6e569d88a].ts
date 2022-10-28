3d6f76593f4df1c148f980a37538734cf83d9286
PirateBook1 <PirateBook1@users.noreply.github.com>
2022-10-28 11:44:45 +0000

    [create-pull-request] automated change

---
+++ patch/articles/[166bba2746][f750c865-cb15-4c03-98b2-aad6e569d88a].ts
@@ -0,0 +1,5 @@
+
+export default [
+// {"version":2,"parts":{"0":{"diff":"=21\t+区司令部《关于今冬明春民兵整组几个问题的意见》的批示","type":"title"},"1":{"delete":true},"2":{"delete":true},"3":{"diff":"=76\t-1\t+屉\t=85\t+岂\t=4"}},"comments":{},"description":""}
+  {"version":2,"parts":{"0":{"diff":"=21\t+%E5%8C%BA%E5%8F%B8%E4%BB%A4%E9%83%A8%E3%80%8A%E5%85%B3%E4%BA%8E%E4%BB%8A%E5%86%AC%E6%98%8E%E6%98%A5%E6%B0%91%E5%85%B5%E6%95%B4%E7%BB%84%E5%87%A0%E4%B8%AA%E9%97%AE%E9%A2%98%E7%9A%84%E6%84%8F%E8%A7%81%E3%80%8B%E7%9A%84%E6%89%B9%E7%A4%BA","type":"title"},"1":{"delete":true},"2":{"delete":true},"3":{"diff":"=76\t-1\t+%E5%B1%89\t=85\t+%E5%B2%82\t=4"}},"comments":{},"description":""},
+];
