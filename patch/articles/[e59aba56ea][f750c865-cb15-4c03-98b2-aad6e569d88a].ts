4b573fddf4a53f60e32c4a733a01fd2e3975eca7
PirateBook1 <PirateBook1@users.noreply.github.com>
2022-10-29 06:15:00 +0000

    [create-pull-request] automated change

---
+++ patch/articles/[e59aba56ea][f750c865-cb15-4c03-98b2-aad6e569d88a].ts
@@ -0,0 +1,5 @@
+
+export default [
+// {"version":2,"parts":{"0":{"insertBefore":[{"type":"title","text":"（十）毛远新对所谓“三位一体”的言论和批示"}],"diff":"=8\t-1\t+远\t=12\t-1\t=9\t-1\t+‘\t=4\t-1\t+’\t=60\t-1\t+舆\t=3"},"1":{"diff":"=27\t-1\t+委\t=6\t+："},"2":{"diff":"=7\t-1\t+‘\t=4\t-1\t+’\t=111"},"3":{"diff":"=63\t+”"},"4":{"delete":true}},"comments":{},"description":""}
+  {"version":2,"parts":{"0":{"insertBefore":[{"type":"title","text":"（十）毛远新对所谓“三位一体”的言论和批示"}],"diff":"=8\t-1\t+%E8%BF%9C\t=12\t-1\t=9\t-1\t+%E2%80%98\t=4\t-1\t+%E2%80%99\t=60\t-1\t+%E8%88%86\t=3"},"1":{"diff":"=27\t-1\t+%E5%A7%94\t=6\t+%EF%BC%9A"},"2":{"diff":"=7\t-1\t+%E2%80%98\t=4\t-1\t+%E2%80%99\t=111"},"3":{"diff":"=63\t+%E2%80%9D"},"4":{"delete":true}},"comments":{},"description":""},
+];
