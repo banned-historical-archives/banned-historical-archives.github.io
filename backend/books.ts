e82d1da257bef73cbe6ec0558b6dedc810c6bae0
PirateBook1 <PirateBook1@users.noreply.github.com>
2022-10-29 14:50:59 +0800

    [create-pull-request] automated change

--- backend/books.ts
+++ backend/books.ts
@@ -48,6 +48,34 @@ const patch_dir = join(normalize(__dirname), '../patch/articles');
 const books: Book[] = [
   {
     entity: {
+      id: 'b99cafbf-6442-4c20-bd92-45f37dbfdcff',
+      name: '王洪文同志讲话',
+      internal: true,
+      official: true,
+      type: 'img',
+      author: '',
+      files: new Array(2)
+        .fill(0)
+        .map(
+          (i, idx) =>
+            `https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/b99cafbf-6442-4c20-bd92-45f37dbfdcff/${
+              idx + 1
+            }.jpg`,
+        )
+        .join(','),
+    },
+    parser_option: {
+      page_limits: [],
+      ext: 'jpg',
+      articles: [{"title":"王洪文同志在沪东造船厂“高举毛泽东思想伟大红旗，以党的政策武装和发动群众，对敌斗争现场会议”的讲话","authors":["王洪文"],"page_start":1,"page_end":2,"dates":[{"year":1968,"month":7,"day":5}]}],
+      ocr: undefined,
+      ocr_exceptions: {},
+    },
+    parser: automation.parse,
+    path: join(normalize(__dirname), '../public/books/archives1/b99cafbf-6442-4c20-bd92-45f37dbfdcff'),
+  },
+  {
+    entity: {
       id: 'e51c3cd5-f427-4fce-ad75-da87f2df5195',
       name: '王洪文同志二月九日在接见四川大足汽车厂在北京学习班同志的讲话',
       internal: true,
