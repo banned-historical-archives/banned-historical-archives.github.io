89c099e4114a728fc85a906e60e4c94ccb59c8d8
PirateBook1 <PirateBook1@users.noreply.github.com>
2022-10-29 06:10:42 +0000

    [create-pull-request] automated change

---
+++ patch/articles/[92bbab67dd][f750c865-cb15-4c03-98b2-aad6e569d88a].ts
@@ -0,0 +1,5 @@
+
+export default [
+// {"version":2,"parts":{"0":{"diff":"=21\t+民兵工作领导小组会议情况时的插话（摘录）","type":"title"},"1":{"delete":true},"2":{"diff":"=50\t-1\t+于\t=18"}},"comments":{},"description":""}
+  {"version":2,"parts":{"0":{"diff":"=21\t+%E6%B0%91%E5%85%B5%E5%B7%A5%E4%BD%9C%E9%A2%86%E5%AF%BC%E5%B0%8F%E7%BB%84%E4%BC%9A%E8%AE%AE%E6%83%85%E5%86%B5%E6%97%B6%E7%9A%84%E6%8F%92%E8%AF%9D%EF%BC%88%E6%91%98%E5%BD%95%EF%BC%89","type":"title"},"1":{"delete":true},"2":{"diff":"=50\t-1\t+%E4%BA%8E\t=18"}},"comments":{},"description":""},
+];
