2b827b3130c8d8322c742d50db51e2aa952bbbb9
PirateBook1 <PirateBook1@users.noreply.github.com>
2022-11-04 09:47:22 +0000

    [create-pull-request] automated change

---
+++ patch/articles/[812f4e2009][7211a45f-26fb-4355-bd7f-fd025d873813].ts
@@ -0,0 +1,5 @@
+
+export default [
+// {"version":2,"parts":{"0":{"type":"title"},"1":{"diff":"=157\t-1\t+己\t=91"},"2":{"diff":"=60\t+。"},"3":{"type":"signature"}},"comments":{},"description":""}
+  {"version":2,"parts":{"0":{"type":"title"},"1":{"diff":"=157\t-1\t+%E5%B7%B1\t=91"},"2":{"diff":"=60\t+%E3%80%82"},"3":{"type":"signature"}},"comments":{},"description":""},
+];
