ae925e42e5d9d3fd9a0309da944e73a0f94049ae
PirateBook1 <PirateBook1@users.noreply.github.com>
2022-11-06 03:27:16 +0000

    [create-pull-request] automated change

---
+++ patch/articles/[5202e55cdb][c21f8808-2334-4e60-a047-27a52e8adbce].ts
@@ -0,0 +1,5 @@
+
+export default [
+// {"version":2,"parts":{"0":{"diff":"-34\t=23","type":"title"},"1":{"diff":"=30\t-1\t=19\t-1\t+抬\t=26\t-1\t+’\t=29\t-2\t+轰轰\t=9\t+”\t=1"},"2":{"diff":"=52\t-1\t+轰\t=10\t-1\t+、\t=69"},"3":{"diff":"=2\t-1\t+霞\t=59\t+”"},"4":{"diff":"=27\t+理和省委领导同志。"}},"comments":{},"description":""}
+  {"version":2,"parts":{"0":{"diff":"-34\t=23","type":"title"},"1":{"diff":"=30\t-1\t=19\t-1\t+%E6%8A%AC\t=26\t-1\t+%E2%80%99\t=29\t-2\t+%E8%BD%B0%E8%BD%B0\t=9\t+%E2%80%9D\t=1"},"2":{"diff":"=52\t-1\t+%E8%BD%B0\t=10\t-1\t+%E3%80%81\t=69"},"3":{"diff":"=2\t-1\t+%E9%9C%9E\t=59\t+%E2%80%9D"},"4":{"diff":"=27\t+%E7%90%86%E5%92%8C%E7%9C%81%E5%A7%94%E9%A2%86%E5%AF%BC%E5%90%8C%E5%BF%97%E3%80%82"}},"comments":{},"description":""},
+];
