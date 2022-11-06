37b0bea5a8ed50d3b5cd3bf2166ab5d2e4c9eb14
PirateBook1 <PirateBook1@users.noreply.github.com>
2022-11-06 03:35:16 +0000

    [create-pull-request] automated change

---
+++ patch/articles/[f6c6a5928e][c21f8808-2334-4e60-a047-27a52e8adbce].ts
@@ -0,0 +1,5 @@
+
+export default [
+// {"version":2,"parts":{"0":{"delete":true},"1":{"type":"title"},"2":{"diff":"=126\t-1\t+束缚\t=73\t-1\t+垮\t=128"}},"comments":{},"description":""}
+  {"version":2,"parts":{"0":{"delete":true},"1":{"type":"title"},"2":{"diff":"=126\t-1\t+%E6%9D%9F%E7%BC%9A\t=73\t-1\t+%E5%9E%AE\t=128"}},"comments":{},"description":""},
+];
