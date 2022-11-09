d3d0e87fb8b2fddb9aacde0d23f70da80b6ebd81
PirateBook1 <news_porter@protonmail.com>
2022-11-09 18:10:33 +0800

    change the date of 事情正在起变化

--- backend/debug.ts
+++ backend/debug.ts
@@ -12,7 +12,7 @@ import { basename } from 'node:path';
 
 (async () => {
   const ds = await init();
-  const book = books.find(i => i.entity.id === '72a130d0-a36b-4b75-8e93-f35bf6ff8176')!;
+  const book = books.find(i => i.entity.id === '097e8dc4-78a1-46bb-8b7e-467bb84632b7')!;
   const res = await book.parser(book.path, book.parser_option);
   console.log(res.map(i => [i, get_article_id(i)]));
 
