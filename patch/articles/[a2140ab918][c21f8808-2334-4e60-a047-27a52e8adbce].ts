0dce84b84f74273431c39e5bab339e7a971c9d16
PirateBook1 <PirateBook1@users.noreply.github.com>
2022-11-09 03:51:33 +0000

    [create-pull-request] automated change

---
+++ patch/articles/[a2140ab918][c21f8808-2334-4e60-a047-27a52e8adbce].ts
@@ -0,0 +1,5 @@
+
+export default [
+// {"version":2,"parts":{"0":{"delete":true},"1":{"diff":"=16\t+集会上的讲话（节录）","type":"title"},"2":{"delete":true},"3":{"diff":"=15\t-1\t+追\t=23\t-1\t+干\t=17\t-1\t+‘\t=7\t-1\t+’\t=8\t-1\t+‘\t=4\t-1\t+’\t=10\t-1\t+追\t=56"},"4":{"diff":"=14\t-1\t+撒\t=59\t+‘\t=12\t-1\t+’\t=44"},"5":{"delete":true},"6":{"delete":true}},"comments":{},"description":""}
+  {"version":2,"parts":{"0":{"delete":true},"1":{"diff":"=16\t+%E9%9B%86%E4%BC%9A%E4%B8%8A%E7%9A%84%E8%AE%B2%E8%AF%9D%EF%BC%88%E8%8A%82%E5%BD%95%EF%BC%89","type":"title"},"2":{"delete":true},"3":{"diff":"=15\t-1\t+%E8%BF%BD\t=23\t-1\t+%E5%B9%B2\t=17\t-1\t+%E2%80%98\t=7\t-1\t+%E2%80%99\t=8\t-1\t+%E2%80%98\t=4\t-1\t+%E2%80%99\t=10\t-1\t+%E8%BF%BD\t=56"},"4":{"diff":"=14\t-1\t+%E6%92%92\t=59\t+%E2%80%98\t=12\t-1\t+%E2%80%99\t=44"},"5":{"delete":true},"6":{"delete":true}},"comments":{},"description":""},
+];
