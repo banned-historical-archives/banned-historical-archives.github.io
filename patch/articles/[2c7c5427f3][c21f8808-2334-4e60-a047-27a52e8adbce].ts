643a79cb96f5de4bb61d4bb6a429ee7dd8937aaf
PirateBook1 <PirateBook1@users.noreply.github.com>
2022-11-09 03:49:29 +0000

    [create-pull-request] automated change

---
+++ patch/articles/[2c7c5427f3][c21f8808-2334-4e60-a047-27a52e8adbce].ts
@@ -0,0 +1,5 @@
+
+export default [
+// {"version":2,"parts":{"0":{"delete":true},"1":{"diff":"=17\t+集会上的讲话（节录）","type":"title"},"2":{"delete":true},"3":{"diff":"=81\t-1\t+沽\t=26\t-1\t+艰\t=151"},"4":{"delete":true},"5":{"delete":true},"6":{"delete":true},"7":{"delete":true}},"comments":{},"description":""}
+  {"version":2,"parts":{"0":{"delete":true},"1":{"diff":"=17\t+%E9%9B%86%E4%BC%9A%E4%B8%8A%E7%9A%84%E8%AE%B2%E8%AF%9D%EF%BC%88%E8%8A%82%E5%BD%95%EF%BC%89","type":"title"},"2":{"delete":true},"3":{"diff":"=81\t-1\t+%E6%B2%BD\t=26\t-1\t+%E8%89%B0\t=151"},"4":{"delete":true},"5":{"delete":true},"6":{"delete":true},"7":{"delete":true}},"comments":{},"description":""},
+];
