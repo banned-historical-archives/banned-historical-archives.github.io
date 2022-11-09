c3cf1cc8f3914b579841531cfbca48f6871e7172
PirateBook1 <PirateBook1@users.noreply.github.com>
2022-11-09 03:56:06 +0000

    [create-pull-request] automated change

---
+++ patch/articles/[125cb0bdfb][c21f8808-2334-4e60-a047-27a52e8adbce].ts
@@ -0,0 +1,5 @@
+
+export default [
+// {"version":2,"parts":{"0":{"delete":true},"1":{"insertBefore":[{"type":"title","text":"七月十三日潘洪斌在市房地局礼堂集会上的讲话（节录）"}],"diff":"-1\t=2\t-1\t+斌\t=6\t-1\t+些\t=1\t-1\t+‘\t=1\t-1\t+’\t=84"},"2":{"delete":true},"3":{"delete":true}},"comments":{},"description":""}
+  {"version":2,"parts":{"0":{"delete":true},"1":{"insertBefore":[{"type":"title","text":"七月十三日潘洪斌在市房地局礼堂集会上的讲话（节录）"}],"diff":"-1\t=2\t-1\t+%E6%96%8C\t=6\t-1\t+%E4%BA%9B\t=1\t-1\t+%E2%80%98\t=1\t-1\t+%E2%80%99\t=84"},"2":{"delete":true},"3":{"delete":true}},"comments":{},"description":""},
+];
