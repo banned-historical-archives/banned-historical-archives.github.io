72b9b56a1cab209994143567a44df590761db532
PirateBook1 <PirateBook1@users.noreply.github.com>
2022-11-12 14:46:16 +0800

    [create-pull-request] automated change

--- backend/books.ts
+++ backend/books.ts
@@ -22,6 +22,34 @@ const patch_dir = join(normalize(__dirname), '../patch/articles');
 const books: Book[] = [
   {
     entity: {
+      id: 'ae99e514-f613-49a6-b3a0-23a0912589cb',
+      name: '在“高举毛泽东思想伟大红旗，进一步开展‘三结合’夺权斗争誓师大会”上张春桥、姚文元同志的讲话',
+      internal: true,
+      official: true,
+      type: 'img',
+      author: '',
+      files: new Array(11)
+        .fill(0)
+        .map(
+          (i, idx) =>
+            `https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/ae99e514-f613-49a6-b3a0-23a0912589cb/${
+              idx + 1
+            }.png`,
+        )
+        .join(','),
+    },
+    parser_option: {
+      page_limits: [],
+      ext: 'png',
+      articles: [{"title":"张春桥姚文元在“高举毛泽东思想伟大红旗进一步开展三结合夺权斗争誓师大会”上的讲话","alias":"张春桥、姚文元同志在“高举毛泽东思想伟大红旗，进一步开展‘三结合’夺权斗争誓师大会”上的讲话","authors":["张春桥","姚文元"],"page_start":2,"page_end":11,"dates":[{"year":1967,"month":2,"day":24}]}],
+      ocr: {"content_thresholds":[0,0.09,0,0]},
+      ocr_exceptions: {},
+    },
+    parser: automation.parse,
+    path: join(normalize(__dirname), '../public/books/archives1/ae99e514-f613-49a6-b3a0-23a0912589cb'),
+  },
+  {
+    entity: {
       id: '7b3123c6-00a9-4c92-b554-15d6dfa8e4e2',
       name: '张春桥、姚文元同志在上海市革命造反派座谈会上的讲话',
       internal: true,
