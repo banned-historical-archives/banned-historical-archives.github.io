ff78d1b2731ba99ea4150052f1498b315d6ee149
PirateBook1 <news_porter@protonmail.com>
2022-11-16 18:52:00 +0800

    add jsons of 61612ccb-a285-40a9-a563-c70499cdd364

--- backend/debug.ts
+++ backend/debug.ts
@@ -12,9 +12,9 @@ import { basename } from 'node:path';
 
 (async () => {
   const ds = await init();
-  const book = books.find(i => i.entity.id === '097e8dc4-78a1-46bb-8b7e-467bb84632b7')!;
+  const book = books.find(i => i.entity.id === '61612ccb-a285-40a9-a563-c70499cdd364')!;
   const res = await book.parser(book.path, book.parser_option);
   console.log(res.map(i => [i, get_article_id(i)]));
 
   debugger;
-})();
+})();  
