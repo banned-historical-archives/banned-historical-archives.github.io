2431d1b0191d99e4d88cdd7480b75f929766438c
PirateBook1 <PirateBook1@users.noreply.github.com>
2022-11-20 14:47:03 +0800

    [create-pull-request] automated change

--- backend/books.ts
+++ backend/books.ts
@@ -22,6 +22,26 @@ const patch_dir = join(normalize(__dirname), '../patch/articles');
 const books: Book[] = [
   {
     entity: {
+      id: '0fcbbaa0-3b65-40fb-902e-678b9fd6e506',
+      name: '共产党宣言（人民出版社1963年版）',
+      internal: true,
+      official: true,
+      type: 'pdf',
+      author: '',
+      files: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/0fcbbaa0-3b65-40fb-902e-678b9fd6e506.pdf',
+    },
+    parser_option: {
+      page_limits: [],
+      ext: 'pdf',
+      articles: [{"title":"1872年德文版序言","authors":["马克思","恩格斯"],"page_start":8,"page_end":10,"dates":[{"year":1872,"month":6,"day":24}]},{"title":"1882年俄文版序言","authors":["马克思","恩格斯"],"page_start":11,"page_end":13,"dates":[{"year":1882,"month":1,"day":21}]},{"title":"1883年德文版序言","authors":["恩格斯"],"page_start":14,"page_end":15,"dates":[{"year":1883,"month":6,"day":28}]},{"title":"1888年英文版序言","authors":["恩格斯"],"page_start":16,"page_end":22,"dates":[{"year":1888,"month":1,"day":30}]},{"title":"1890年德文版序言","authors":["恩格斯"],"page_start":23,"page_end":30,"dates":[{"year":1890,"month":5,"day":1}]},{"title":"1892年波兰文版序言","authors":["恩格斯"],"page_start":31,"page_end":33,"dates":[{"year":1892,"month":2,"day":10}]},{"title":"1893年意大利文版序言 告意大利读者","authors":["恩格斯"],"page_start":34,"page_end":36,"dates":[{"year":1893,"month":2,"day":1}]},{"title":"1893年意大利文版序言 告意大利读者","authors":["恩格斯"],"page_start":34,"page_end":36,"dates":[{"year":1893,"month":2,"day":1}]},{"title":"共产党宣言","authors":["马克思","恩格斯"],"page_start":38,"page_end":93,"is_range_date":true,"dates":[{"year":1847,"month":12},{"year":1848,"month":1}]}],
+      ocr: {"content_thresholds":[0,0.11,0,0],"standard_paragraph_merge_strategy_threshold":0.17,"differential_paragraph_merge_strategy_threshold":0},
+      ocr_exceptions: {},
+    },
+    parser: automation.parse,
+    path: join(normalize(__dirname), '../public/books/archives4/0fcbbaa0-3b65-40fb-902e-678b9fd6e506.pdf'),
+  },
+  {
+    entity: {
       id: '9396d223-50a3-44f6-990b-88dbf5b0aec8',
       name: '红旗一九六七年第四期',
       internal: true,
