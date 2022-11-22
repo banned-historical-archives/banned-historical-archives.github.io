91d43cfa9741e9c5b70fecbd5aac799c152cb3d8
PirateBook1 <PirateBook1@users.noreply.github.com>
2022-11-22 13:39:16 +0800

    [create-pull-request] automated change

--- backend/books.ts
+++ backend/books.ts
@@ -22,6 +22,26 @@ const patch_dir = join(normalize(__dirname), '../patch/articles');
 const books: Book[] = [
   {
     entity: {
+      id: '187473e3-da31-44f3-95e9-8b7b89c95627',
+      name: '全国农业劳动模范陈以梅同志在山东省工农兵代表批林批孔汇报大会上的讲话',
+      internal: true,
+      official: true,
+      type: 'pdf',
+      author: '',
+      files: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/187473e3-da31-44f3-95e9-8b7b89c95627.pdf',
+    },
+    parser_option: {
+      page_limits: [],
+      ext: 'pdf',
+      articles: [{"title":"全国农业劳动模范陈以梅同志在山东省工农兵代表批林批孔汇报大会上的讲话","authors":["陈以梅"],"page_start":1,"page_end":7,"dates":[{"year":1974,"month":2,"day":27}]}],
+      ocr: {"standard_paragraph_merge_strategy_threshold":0.186,"differential_paragraph_merge_strategy_threshold":0},
+      ocr_exceptions: {},
+    },
+    parser: automation.parse,
+    path: join(normalize(__dirname), '../public/books/archives1/187473e3-da31-44f3-95e9-8b7b89c95627.pdf'),
+  },
+  {
+    entity: {
       id: '61ae9a00-9153-4e4e-9b79-f8aeb954c7e9',
       name: '马列毛基本教程',
       internal: true,
