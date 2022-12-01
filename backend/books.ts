000b950fdee21ba51920d5b7c7363ffc0b2510bf
PirateBook1 <PirateBook1@users.noreply.github.com>
2022-12-01 15:19:07 +0800

    [create-pull-request] automated change

--- backend/books.ts
+++ backend/books.ts
@@ -22,6 +22,26 @@ const patch_dir = join(normalize(__dirname), '../patch/articles');
 const books: Book[] = [
   {
     entity: {
+      id: '0aa42889-8ffa-4c59-8328-f5fecb4c8bc5',
+      name: '作为发展人民战争的一部分，进行秘鲁共产党的全面重组，以在全国夺取政权！抓住贡萨罗思想解决新问题',
+      internal: true,
+      official: true,
+      type: 'pdf',
+      author: '',
+      files: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives3/main/0aa42889-8ffa-4c59-8328-f5fecb4c8bc5.pdf',
+    },
+    parser_option: {
+      page_limits: [],
+      ext: 'pdf',
+      articles: [{"title":"作为发展人民战争的一部分，进行秘鲁共产党的全面重组，以在全国夺取政权！抓住贡萨罗思想解决新问题","authors":["新民主主义协会"],"page_start":1,"page_end":22,"dates":[{"year":2011,"month":10}]}],
+      ocr: {"standard_paragraph_merge_strategy_threshold":0.169,"differential_paragraph_merge_strategy_threshold":0},
+      ocr_exceptions: {},
+    },
+    parser: automation.parse,
+    path: join(normalize(__dirname), '../public/books/archives3/0aa42889-8ffa-4c59-8328-f5fecb4c8bc5.pdf'),
+  },
+  {
+    entity: {
       id: '18e2f682-cab0-46c1-8938-73d651dfe1a9',
       name: '论贡萨罗的普适贡献',
       internal: true,
