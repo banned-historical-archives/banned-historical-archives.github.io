7c9d6ec20641094accb2c6f47d618d4f9f476df6
banned-historical-archives <banned-historical-archives@users.noreply.github.com>
2022-12-23 10:24:44 +0800

    [create-pull-request] automated change

--- backend/books.ts
+++ backend/books.ts
@@ -22,6 +22,26 @@ const patch_dir = join(normalize(__dirname), '../patch/articles');
 const books: Book[] = [
   {
     entity: {
+      id: 'e8f644e6-1143-4765-8a31-48d38f7602e8',
+      name: '持久人民战争：革命的唯一途径',
+      internal: true,
+      official: true,
+      type: 'pdf',
+      author: '',
+      files: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives3/main/e8f644e6-1143-4765-8a31-48d38f7602e8.pdf',
+    },
+    parser_option: {
+      page_limits: [],
+      ext: 'pdf',
+      articles: [{"title":"持久人民战争：革命的唯一途径","authors":["加拿大革命共产党"],"page_start":2,"page_end":44,"dates":[{"year":2002}]}],
+      ocr: {"standard_paragraph_merge_strategy_threshold":0.178,"differential_paragraph_merge_strategy_threshold":0},
+      ocr_exceptions: {},
+    },
+    parser: automation.parse,
+    path: join(normalize(__dirname), '../public/archives3/e8f644e6-1143-4765-8a31-48d38f7602e8.pdf'),
+  },
+  {
+    entity: {
       id: '7a8f3b48-1107-4235-b6cc-0b092aacb163',
       name: '红旗一九六七年第十一期',
       internal: true,
