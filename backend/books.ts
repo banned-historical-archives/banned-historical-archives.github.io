f42d5e6871ddfbfc550f25ac077c8d159be1d0d7
PirateBook1 <PirateBook1@users.noreply.github.com>
2022-12-31 20:39:15 +0800

    [create-pull-request] automated change

--- backend/books.ts
+++ backend/books.ts
@@ -22,6 +22,26 @@ const patch_dir = join(normalize(__dirname), '../patch/articles');
 const books: Book[] = [
   {
     entity: {
+      id: 'a8ea4e1d-af66-4294-8979-618e5faa703e',
+      name: '路易·波拿巴的雾月十八日（人民出版社1972年版）',
+      internal: true,
+      official: true,
+      type: 'pdf',
+      author: '',
+      files: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/a8ea4e1d-af66-4294-8979-618e5faa703e.pdf',
+    },
+    parser_option: {
+      page_limits: [],
+      ext: 'pdf',
+      articles: [{"title":"卡·马克思为本书第二版作的序言","authors":["马克思"],"page_start":4,"page_end":6,"dates":[{"year":1869,"month":6,"day":23}]},{"title":"弗·恩格斯为本书第三版作的序言","authors":["恩格斯"],"page_start":7,"page_end":8,"dates":[{"year":1885}]},{"title":"路易·波拿巴的雾月十八日","authors":["马克思"],"page_start":10,"page_end":117,"dates":[{"year":1851,"month":12},{"year":1852,"month":3}]},{"title":"注释","authors":[],"page_start":118,"page_end":125,"dates":[]}],
+      ocr: {"content_thresholds":[0.132,0,0,0]},
+      ocr_exceptions: {},
+    },
+    parser: automation.parse,
+    path: join(normalize(__dirname), '../public/archives4/a8ea4e1d-af66-4294-8979-618e5faa703e.pdf'),
+  },
+  {
+    entity: {
       id: 'f5841312-d8c2-4fcf-91a1-9f7c1074de05',
       name: '法兰西内战（人民出版社1971年版）',
       internal: true,
