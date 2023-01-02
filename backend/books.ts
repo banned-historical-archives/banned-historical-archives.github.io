fb21d295ab889ee5e2b06d89b0170f2b7d6a90c0
PirateBook1 <PirateBook1@users.noreply.github.com>
2023-01-02 20:15:38 +0800

    [create-pull-request] automated change

--- backend/books.ts
+++ backend/books.ts
@@ -22,6 +22,26 @@ const patch_dir = join(normalize(__dirname), '../patch/articles');
 const books: Book[] = [
   {
     entity: {
+      id: '6869e951-bde4-44a1-8348-dc6fc27cc26a',
+      name: '青年团的任务（人民出版社1973年版）',
+      internal: true,
+      official: true,
+      type: 'pdf',
+      author: '',
+      files: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives5/main/6869e951-bde4-44a1-8348-dc6fc27cc26a.pdf',
+    },
+    parser_option: {
+      page_limits: [],
+      ext: 'pdf',
+      articles: [{"title":"青年团的任务","authors":["列宁"],"page_start":4,"page_end":22,"dates":[{"year":1920,"month":10,"day":5},{"year":1920,"month":10,"day":6},{"year":1920,"month":10,"day":7}]}],
+      ocr: {"content_thresholds":[0,0.079,0,0]},
+      ocr_exceptions: {},
+    },
+    parser: automation.parse,
+    path: join(normalize(__dirname), '../public/archives5/6869e951-bde4-44a1-8348-dc6fc27cc26a.pdf'),
+  },
+  {
+    entity: {
       id: '959447a7-19ca-480b-b042-22c56f4bdb03',
       name: '国家与革命（人民出版社1976年版）',
       internal: true,
