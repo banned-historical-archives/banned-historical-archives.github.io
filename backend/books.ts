1271a459674dd721cbf22a3d90d67f77c5e882c7
PirateBook1 <3572336885@protonmail.com>
2023-01-17 21:38:10 +0800

    added 3 books which can't upload on browser because of the 25mb restriction

--- backend/books.ts
+++ backend/books.ts
@@ -22,6 +22,68 @@ const patch_dir = join(normalize(__dirname), '../patch/articles');
 const books: Book[] = [
   {
     entity: {
+      id: 'a05b0c2f-7584-4f45-a93d-1bc287752315',
+      name: '简明世界史（近代部分）（北京大学历史系1974年版）',
+      internal: true,
+      official: true,
+      type: 'pdf',
+      author: '',
+      files: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives3/main/a05b0c2f-7584-4f45-a93d-1bc287752315.pdf',
+    },
+    parser_option: {
+      page_limits: [],
+      ext: 'pdf',
+      articles: [{"title":"近代部分引言","authors":["北京大学历史系"],"page_start":15,"page_end":20,"dates":[{"year":1974,"month":10}]},{"title":"第一章 英国资产阶级革命 美国独立战争","authors":["北京大学历史系"],"page_start":21,"page_end":71,"dates":[{"year":1974,"month":10}]},{"title":"第二章 法国资产阶级革命 拿破仑帝国的兴亡","authors":["北京大学历史系"],"page_start":72,"page_end":121,"dates":[{"year":1974,"month":10}]},{"title":"第三章 拉丁美洲独立革命","authors":["北京大学历史系"],"page_start":122,"page_end":144,"dates":[{"year":1974,"month":10}]},{"title":"第四章 十七——十九世纪初亚非人民反对殖民主义、封建主义的斗争","authors":["北京大学历史系"],"page_start":145,"page_end":187,"dates":[{"year":1974,"month":10}]},{"title":"第五章 马克思主义的产生 《共产党宣言》","authors":["北京大学历史系"],"page_start":188,"page_end":222,"dates":[{"year":1974,"month":10}]},{"title":"第六章 一八四八年欧洲革命与五十、六十年代的资产阶级民族民主运动","authors":["北京大学历史系"],"page_start":223,"page_end":271,"dates":[{"year":1974,"month":10}]},{"title":"第七章 十九世纪中叶俄国、美国和日本的资产阶级改革和革命运动","authors":["北京大学历史系"],"page_start":272,"page_end":314,"dates":[{"year":1974,"month":10}]},{"title":"第八章 十九世纪中期前后亚洲各国人民反殖民主义反封建主义的斗争","authors":["北京大学历史系"],"page_start":315,"page_end":338,"dates":[{"year":1974,"month":10}]},{"title":"第九章 第一国际 巴黎公社——人类历史上第一个无产阶级政权","authors":["北京大学历史系"],"page_start":339,"page_end":377,"dates":[{"year":1974,"month":10}]},{"title":"第十章 从“自由”资本主义向帝国主义过渡时期的国际工人运动 马克思恩格斯的革命路线反对机会主义路线的斗争","authors":["北京大学历史系"],"page_start":378,"page_end":421,"dates":[{"year":1974,"month":10}]},{"title":"第十一章 资本主义进入帝国主义阶段 帝国主义初期的国际关系","authors":["北京大学历史系"],"page_start":422,"page_end":459,"dates":[{"year":1974,"month":10}]},{"title":"第十二章 列宁主义的产生 一九〇五年俄国革命 列宁在国际舞台上反对修正主义的斗争","authors":["北京大学历史系"],"page_start":460,"page_end":509,"dates":[{"year":1974,"month":10}]},{"title":"第十三章 十九世纪中期——二十世纪初的非洲和非洲人民反抗殖民主义侵略的斗争","authors":["北京大学历史系"],"page_start":510,"page_end":611,"dates":[{"year":1974,"month":10}]},{"title":"第十五章 独立后至二十世纪初的拉丁美洲 古巴独立战争和墨西哥革命","authors":["北京大学历史系"],"page_start":612,"page_end":636,"dates":[{"year":1974,"month":10}]},{"title":"第十六章 第一次帝国主义世界大列宁反对社会沙文主义的斗争","authors":["北京大学历史系"],"page_start":637,"page_end":663,"dates":[{"year":1974,"month":10}]},{"title":"大事年表","authors":["北京大学历史系"],"page_start":665,"page_end":687,"dates":[{"year":1974,"month":10}]},{"title":"人名中外文对照表","authors":["北京大学历史系"],"page_start":688,"page_end":721,"dates":[{"year":1974,"month":10}]},{"title":"插图","authors":["北京大学历史系"],"page_start":722,"page_end":745,"dates":[{"year":1974,"month":10}]},{"title":"编者说明","authors":["北京大学历史系"],"page_start":748,"page_end":749,"dates":[{"year":1974,"month":8}]}],
+      ocr: {"content_thresholds":[0.132,0,0,0]},
+      ocr_exceptions: {},
+    },
+    parser: automation.parse,
+    path: join(normalize(__dirname), '../public/archives3/a05b0c2f-7584-4f45-a93d-1bc287752315.pdf'),
+  },
+  {
+    entity: {
+      id: '3ff98cd8-969a-4913-af83-638fd6f0514d',
+      name: '近代中国史稿（下）（人民出版社1976年）',
+      internal: true,
+      official: true,
+      type: 'pdf',
+      author: '',
+      files: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives3/main/3ff98cd8-969a-4913-af83-638fd6f0514d.pdf',
+    },
+    parser_option: {
+      page_limits: [],
+      ext: 'pdf',
+      articles: [{"title":"第八章 戊戌维新运动","authors":["《近代中国史稿》编写组"],"page_start":7,"page_end":83,"dates":[{"year":1976,"month":12}]},{"title":"第九章 伟大的义和团反帝爱国运动","authors":["《近代中国史稿》编写组"],"page_start":84,"page_end":176,"dates":[{"year":1976,"month":12}]},{"title":"第十章 粉碎沙俄的“黄俄罗斯”迷梦和抗击英、俄侵略西藏","authors":["《近代中国史稿》编写组"],"page_start":177,"page_end":221,"dates":[{"year":1976,"month":12}]},{"title":"第十一章 资产阶级民主革命运动的高涨","authors":["《近代中国史稿》编写组"],"page_start":222,"page_end":314,"dates":[{"year":1976,"month":12}]},{"title":"第十二章 推翻帝国主义走狗清朝的辛亥革命","authors":["《近代中国史稿》编写组"],"page_start":315,"page_end":400,"dates":[{"year":1976,"month":12}]},{"title":"第十三章 反对帝国主义走狗北洋军阀的斗争","authors":["《近代中国史稿》编写组"],"page_start":401,"page_end":514,"dates":[{"year":1976,"month":12}]}],
+      ocr: {"standard_paragraph_merge_strategy_threshold": 0.18,
+        "differential_paragraph_merge_strategy_threshold": 0,"content_thresholds":[0,0.09,0,0]},
+      ocr_exceptions: {},
+    },
+    parser: automation.parse,
+    path: join(normalize(__dirname), '../public/archives3/3ff98cd8-969a-4913-af83-638fd6f0514d.pdf'),
+  },
+  {
+    entity: {
+      id: '506753a3-f856-4446-8bf3-37f7ec15b974',
+      name: '近代中国史稿（上）（人民出版社1976年）',
+      internal: true,
+      official: true,
+      type: 'pdf',
+      author: '',
+      files: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives3/main/506753a3-f856-4446-8bf3-37f7ec15b974.pdf',
+    },
+    parser_option: {
+      page_limits: [],
+      ext: 'pdf',
+      articles: [{"title":"第一章 反对英国侵略的鸦片战争","authors":["《近代中国史稿》编写组"],"page_start":19,"page_end":106,"dates":[{"year":1976,"month":12}]},{"title":"第二章 蓬勃兴起的太平天国革命","authors":["《近代中国史稿》编写组"],"page_start":107,"page_end":191,"dates":[{"year":1976,"month":12}]},{"title":"第三章 反对英法联军侵略和沙俄鲸吞中国大片领土的斗争","authors":["《近代中国史稿》编写组"],"page_start":192,"page_end":256,"dates":[{"year":1976,"month":12}]},{"title":"第四章 太平天国英雄们为保卫农民政权而斗争","authors":["《近代中国史稿》编写组"],"page_start":257,"page_end":337,"dates":[{"year":1976,"month":12}]},{"title":"第五章 中国社会加速半殖民地半封建化，早期的无产阶级和资产阶级","authors":["《近代中国史稿》编写组"],"page_start":338,"page_end":394,"dates":[{"year":1976,"month":12}]},{"title":"第六章 保卫祖国边疆，反对法国侵略的中法战争","authors":["《近代中国史稿》编写组"],"page_start":395,"page_end":446,"dates":[{"year":1976,"month":12}]},{"title":"第七章 反对日本侵略的甲午战争","authors":["《近代中国史稿》编写组"],"page_start":447,"page_end":518,"dates":[{"year":1976,"month":12}]}],
+      ocr: {"standard_paragraph_merge_strategy_threshold": 0.18,
+        "differential_paragraph_merge_strategy_threshold": 0,"content_thresholds":[0,0.09,0,0]},
+      ocr_exceptions: {},
+    },
+    parser: automation.parse,
+    path: join(normalize(__dirname), '../public/archives3/506753a3-f856-4446-8bf3-37f7ec15b974.pdf'),
+  },
+  {
+    entity: {
       id: '5a6e14f4-1103-48c3-bd99-37613918dd4b',
       name: '中共党史教学参考资料 第25册',
       internal: true,
