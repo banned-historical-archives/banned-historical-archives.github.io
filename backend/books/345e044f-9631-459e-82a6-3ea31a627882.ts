3644dd80542853e27bdbed05d149e4dc893be1c5
banned-historical-archives <banned-historical-archives@users.noreply.github.com>
2023-01-25 11:10:01 +0800

    [create-pull-request] automated change

---
+++ backend/books/345e044f-9631-459e-82a6-3ea31a627882.ts
@@ -0,0 +1,29 @@
+export default {
+  entity: {
+    id: '345e044f-9631-459e-82a6-3ea31a627882',
+    name: '无产阶级文化大革命参考资料-test',
+    internal: false,
+    official: false,
+    type: 'pdf',
+    author: '',
+    files:
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives3/main/345e044f-9631-459e-82a6-3ea31a627882.pdf',
+  },
+  parser_option: {
+    page_limits: [],
+    ext: 'pdf',
+    articles: [
+      {
+        title: '目录',
+        authors: ['毛泽东', '江青'],
+        page_start: 1,
+        page_end: 10,
+        dates: [{ year: 1949, month: 10, day: 1 }],
+      },
+    ],
+    ocr: undefined,
+    ocr_exceptions: {},
+  },
+  parser_id: 'automation',
+  path: '/archives3/345e044f-9631-459e-82a6-3ea31a627882.pdf',
+};
