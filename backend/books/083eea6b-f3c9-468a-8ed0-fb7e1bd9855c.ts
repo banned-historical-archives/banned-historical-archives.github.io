30bd67c61b2ecc2db50107b21cd87a82df19b9ff
banned-historical-archives <banned-historical-archives@users.noreply.github.com>
2023-01-26 22:02:00 +0800

    [create-pull-request] automated change

---
+++ backend/books/083eea6b-f3c9-468a-8ed0-fb7e1bd9855c.ts
@@ -0,0 +1,29 @@
+export default {
+  entity: {
+    id: '083eea6b-f3c9-468a-8ed0-fb7e1bd9855c',
+    name: 'wansui5',
+    internal: false,
+    official: false,
+    type: 'pdf',
+    author: '',
+    files:
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/083eea6b-f3c9-468a-8ed0-fb7e1bd9855c.pdf',
+  },
+  parser_option: {
+    page_limits: [],
+    ext: 'pdf',
+    articles: [
+      {
+        title: 'mulu',
+        authors: ['毛泽东'],
+        page_start: 1,
+        page_end: 13,
+        dates: [{ year: 1949, month: 10, day: 1 }],
+      },
+    ],
+    ocr: undefined,
+    ocr_exceptions: {},
+  },
+  parser_id: 'automation',
+  path: '/archives1/083eea6b-f3c9-468a-8ed0-fb7e1bd9855c.pdf',
+};
