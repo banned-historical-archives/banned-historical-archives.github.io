223656da96b0746fe16269a263d393e3e939adf7
PirateBook1 <PirateBook1@users.noreply.github.com>
2023-01-28 15:33:32 +0800

    [create-pull-request] automated change

---
+++ backend/books/adfa8c33-f1fb-4b82-a1e3-d636a2b875ad.ts
@@ -0,0 +1,180 @@
+export default {
+  entity: {
+    id: 'adfa8c33-f1fb-4b82-a1e3-d636a2b875ad',
+    name: '云南文革史稿（下）（美国华忆出版社2020年）',
+    internal: false,
+    official: false,
+    type: 'pdf',
+    author: '周孜仁著，美国华忆出版社2020年',
+    files:
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives3/main/adfa8c33-f1fb-4b82-a1e3-d636a2b875ad.pdf',
+  },
+  parser_option: {
+    page_limits: [],
+    ext: 'pdf',
+    articles: [
+      {
+        title:
+          '卷三 谭甫仁时代：热昏岁月（1968年8月13日-1970年2月17日） 卷首导读',
+        authors: ['周孜仁'],
+        page_start: 15,
+        page_end: 18,
+        dates: [{ year: 2020, month: 3 }],
+      },
+      {
+        title:
+          '卷三 谭甫仁时代：热昏岁月（1968年8月13日-1970年2月17日） 第一章 加热仇恨',
+        authors: ['周孜仁'],
+        page_start: 19,
+        page_end: 39,
+        dates: [{ year: 2020, month: 3 }],
+      },
+      {
+        title:
+          '卷三 谭甫仁时代：热昏岁月（1968年8月13日-1970年2月17日） 第二章 七次全会的会里会外',
+        authors: ['周孜仁'],
+        page_start: 40,
+        page_end: 62,
+        dates: [{ year: 2020, month: 3 }],
+      },
+      {
+        title:
+          '卷三 谭甫仁时代：热昏岁月（1968年8月13日-1970年2月17日） 第三章 边疆“新秩序”',
+        authors: ['周孜仁'],
+        page_start: 63,
+        page_end: 88,
+        dates: [{ year: 2020, month: 3 }],
+      },
+      {
+        title:
+          '卷三 谭甫仁时代：热昏岁月（1968年8月13日-1970年2月17日） 第四章 暗流',
+        authors: ['周孜仁'],
+        page_start: 89,
+        page_end: 100,
+        dates: [{ year: 2020, month: 3 }],
+      },
+      {
+        title:
+          '卷三 谭甫仁时代：热昏岁月（1968年8月13日-1970年2月17日） 第五章 “017凶杀案”',
+        authors: ['周孜仁'],
+        page_start: 101,
+        page_end: 108,
+        dates: [{ year: 2020, month: 3 }],
+      },
+      {
+        title:
+          '卷四 周兴时代：破船负重泛中流（1970年12月18日-1975年10月3日） 第一章 从“批陈”到“913”',
+        authors: ['周孜仁'],
+        page_start: 111,
+        page_end: 128,
+        dates: [{ year: 2020, month: 3 }],
+      },
+      {
+        title:
+          '卷四 周兴时代：破船负重泛中流（1970年12月18日-1975年10月3日） 第二章 “批林整风”云南',
+        authors: ['周孜仁'],
+        page_start: 129,
+        page_end: 150,
+        dates: [{ year: 2020, month: 3 }],
+      },
+      {
+        title:
+          '卷四 周兴时代：破船负重泛中流（1970年12月18日-1975年10月3日） 第三章 “批林批孔”：“66乱局”蹩脚的微缩版',
+        authors: ['周孜仁'],
+        page_start: 151,
+        page_end: 183,
+        dates: [{ year: 2020, month: 3 }],
+      },
+      {
+        title:
+          '卷四 周兴时代：破船负重泛中流（1970年12月18日-1975年10月3日） 第四章 云南民族关系的血色之殇（上）',
+        authors: ['周孜仁'],
+        page_start: 184,
+        page_end: 196,
+        dates: [{ year: 2020, month: 3 }],
+      },
+      {
+        title:
+          '卷四 周兴时代：破船负重泛中流（1970年12月18日-1975年10月3日） 第五章 “100天会议”和26号文件',
+        authors: ['周孜仁'],
+        page_start: 197,
+        page_end: 221,
+        dates: [{ year: 2020, month: 3 }],
+      },
+      {
+        title:
+          '卷四 周兴时代：破船负重泛中流（1970年12月18日-1975年10月3日） 第六章 云南民族关系的血色之殇（下）',
+        authors: ['周孜仁'],
+        page_start: 222,
+        page_end: 229,
+        dates: [{ year: 2020, month: 3 }],
+      },
+      {
+        title:
+          '卷五 贾启允时代：难以维持的“维持会”（1975年10月5日-1977年1月） 第一章 仓促的序章',
+        authors: ['周孜仁'],
+        page_start: 233,
+        page_end: 241,
+        dates: [{ year: 2020, month: 3 }],
+      },
+      {
+        title:
+          '卷五 贾启允时代：难以维持的“维持会”（1975年10月5日-1977年1月） 第二章 乱局3.0版',
+        authors: ['周孜仁'],
+        page_start: 242,
+        page_end: 271,
+        dates: [{ year: 2020, month: 3 }],
+      },
+      {
+        title:
+          '卷五 贾启允时代：难以维持的“维持会”（1975年10月5日-1977年1月） 第三章 绝响',
+        authors: ['周孜仁'],
+        page_start: 272,
+        page_end: 278,
+        dates: [{ year: 2020, month: 3 }],
+      },
+      {
+        title:
+          '卷六 后文革时代：无法告别的“继续革命”（1976年10月—） 第一章 权力依旧傲慢',
+        authors: ['周孜仁'],
+        page_start: 281,
+        page_end: 296,
+        dates: [{ year: 2020, month: 3 }],
+      },
+      {
+        title:
+          '卷六 后文革时代：无法告别的“继续革命”（1976年10月—） 第二章 造反派的末日',
+        authors: ['周孜仁'],
+        page_start: 297,
+        page_end: 313,
+        dates: [{ year: 2020, month: 3 }],
+      },
+      {
+        title:
+          '卷六 后文革时代：无法告别的“继续革命”（1976年10月—） 第三章 最后的挽歌',
+        authors: ['周孜仁'],
+        page_start: 314,
+        page_end: 333,
+        dates: [{ year: 2020, month: 3 }],
+      },
+      {
+        title: '跋：亲历者的使命',
+        authors: ['周孜仁'],
+        page_start: 335,
+        page_end: 337,
+        dates: [{ year: 2019, month: 10, day: 3 }],
+      },
+      {
+        title: '下卷参考文献',
+        authors: ['周孜仁'],
+        page_start: 339,
+        page_end: 342,
+        dates: [{ year: 2019, month: 10, day: 3 }],
+      },
+    ],
+    ocr: { content_thresholds: [0.1, 0.1, 0, 0] },
+    ocr_exceptions: {},
+  },
+  parser_id: 'automation',
+  path: '/archives3/adfa8c33-f1fb-4b82-a1e3-d636a2b875ad.pdf',
+};
