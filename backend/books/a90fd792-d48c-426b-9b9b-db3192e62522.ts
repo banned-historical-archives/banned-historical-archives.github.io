0305dc7b6b0e3bb64325cccdcce28f964825e0e1
PirateBook1 <PirateBook1@users.noreply.github.com>
2023-01-29 15:46:41 +0800

    [create-pull-request] automated change

---
+++ backend/books/a90fd792-d48c-426b-9b9b-db3192e62522.ts
@@ -0,0 +1,290 @@
+export default {
+  entity: {
+    id: 'a90fd792-d48c-426b-9b9b-db3192e62522',
+    name: '武汉地区文革纪实（中国文化传播出版社2020年）',
+    internal: false,
+    official: false,
+    type: 'pdf',
+    author: '中国文化传播出版社2020年',
+    files:
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives3/main/a90fd792-d48c-426b-9b9b-db3192e62522.pdf',
+  },
+  parser_option: {
+    page_limits: [],
+    ext: 'pdf',
+    articles: [
+      {
+        title: '引子 走向文革：1962年-1966年5月的武汉',
+        authors: ['新世纪出版社'],
+        page_start: 14,
+        page_end: 44,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第一编 武汉文革的全面发动（1966年4月-1967年1月） 第一章 文革初期的“五十天”',
+        authors: ['新世纪出版社'],
+        page_start: 47,
+        page_end: 79,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第一编 武汉文革的全面发动（1966年4月-1967年1月） 第二章 工作组、三员、文革委员会与少数派',
+        authors: ['新世纪出版社'],
+        page_start: 80,
+        page_end: 94,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第一编 武汉文革的全面发动（1966年4月-1967年1月） 第三章 红卫兵成立与“破四旧”',
+        authors: ['新世纪出版社'],
+        page_start: 95,
+        page_end: 117,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第一编 武汉文革的全面发动（1966年4月-1967年1月） 第四章 “揪南下一小撮”',
+        authors: ['新世纪出版社'],
+        page_start: 118,
+        page_end: 146,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第一编 武汉文革的全面发动（1966年4月-1967年1月） 第五章 武汉地区师生赴京串连',
+        authors: ['新世纪出版社'],
+        page_start: 147,
+        page_end: 157,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第一编 武汉文革的全面发动（1966年4月-1967年1月） 第六章 批判资产阶级反动路线',
+        authors: ['新世纪出版社'],
+        page_start: 158,
+        page_end: 185,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第一编 武汉文革的全面发动（1966年4月-1967年1月） 第七章 全市性学生造反组织成立',
+        authors: ['新世纪出版社'],
+        page_start: 186,
+        page_end: 203,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第一编 武汉文革的全面发动（1966年4月-1967年1月） 第八章 造反派封闭湖北日报',
+        authors: ['新世纪出版社'],
+        page_start: 204,
+        page_end: 217,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第一编 武汉文革的全面发动（1966年4月-1967年1月） 第九章 工人造反组织成立',
+        authors: ['新世纪出版社'],
+        page_start: 218,
+        page_end: 248,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第一编 武汉文革的全面发动（1966年4月-1967年1月） 第十章 职工联合会从成立到解散',
+        authors: ['新世纪出版社'],
+        page_start: 249,
+        page_end: 261,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第一编 武汉文革的全面发动（1966年4月-1967年1月） 第十一章 批斗王任重',
+        authors: ['新世纪出版社'],
+        page_start: 262,
+        page_end: 271,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第二编 “一月革命”与“二月逆流”、“七二〇事件”（1967年1月-7月） 第十二章 一月夺权',
+        authors: ['新世纪出版社'],
+        page_start: 273,
+        page_end: 306,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第二编 “一月革命”与“二月逆流”、“七二〇事件”（1967年1月-7月） 第十三章 “二月逆流”引发的“三月黑风”',
+        authors: ['新世纪出版社'],
+        page_start: 307,
+        page_end: 335,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第二编 “一月革命”与“二月逆流”、“七二〇事件”（1967年1月-7月） 第十四章 "揪‘武老谭’"与“牛鬼蛇神一扫光”',
+        authors: ['新世纪出版社'],
+        page_start: 336,
+        page_end: 379,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第二编 “一月革命”与“二月逆流”、“七二〇事件”（1967年1月-7月） 第十五章 血腥的6月',
+        authors: ['新世纪出版社'],
+        page_start: 380,
+        page_end: 421,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第二编 “一月革命”与“二月逆流”、“七二〇事件”（1967年1月-7月） 第十六章 七二〇事件',
+        authors: ['新世纪出版社'],
+        page_start: 422,
+        page_end: 485,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第三编 武汉军区支持造反派的一年（1967年7月-1968年8月） 第十七章 七二〇事件后的武汉局势',
+        authors: ['新世纪出版社'],
+        page_start: 487,
+        page_end: 503,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第三编 武汉军区支持造反派的一年（1967年7月-1968年8月） 第十八章 实现大联合，成立革委会',
+        authors: ['新世纪出版社'],
+        page_start: 504,
+        page_end: 548,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第三编 武汉军区支持造反派的一年（1967年7月-1968年8月） 第十九章 “三反一粉碎”中的钢新之争',
+        authors: ['新世纪出版社'],
+        page_start: 549,
+        page_end: 562,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第四编 从“清队”到“两清一批”（1968年8月-1971年10月） 第二十章 清理阶级队伍与工人解放军宣传队进驻',
+        authors: ['新世纪出版社'],
+        page_start: 565,
+        page_end: 585,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第四编 从“清队”到“两清一批”（1968年8月-1971年10月） 第二十一章 “反复旧”——最后的大规模群众运动',
+        authors: ['新世纪出版社'],
+        page_start: 586,
+        page_end: 602,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第四编 从“清队”到“两清一批”（1968年8月-1971年10月） 第二十二章 “两清一批”',
+        authors: ['新世纪出版社'],
+        page_start: 603,
+        page_end: 643,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第五编 批林批孔——全面治理整顿——反击右倾翻案风（1971年10月-1976年9月） 第二十三章 “批林整风”：相对平静的两年',
+        authors: ['新世纪出版社'],
+        page_start: 645,
+        page_end: 658,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第五编 批林批孔——全面治理整顿——反击右倾翻案风（1971年10月-1976年9月） 第二十四章 武汉地区的“反潮流”运动',
+        authors: ['新世纪出版社'],
+        page_start: 659,
+        page_end: 692,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第五编 批林批孔——全面治理整顿——反击右倾翻案风（1971年10月-1976年9月） 第二十五章 武汉地区的批林批孔运动',
+        authors: ['新世纪出版社'],
+        page_start: 693,
+        page_end: 746,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第五编 批林批孔——全面治理整顿——反击右倾翻案风（1971年10月-1976年9月） 第二十六章 1975年的全面治理整顿',
+        authors: ['新世纪出版社'],
+        page_start: 747,
+        page_end: 757,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第五编 批林批孔——全面治理整顿——反击右倾翻案风（1971年10月-1976年9月） 第二十七章 批邓、反击右倾翻案风',
+        authors: ['新世纪出版社'],
+        page_start: 758,
+        page_end: 781,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第六编 文革被彻底否定（1976年9月-） 第二十八章 毛泽东逝世和“四人帮”被捕',
+        authors: ['新世纪出版社'],
+        page_start: 783,
+        page_end: 806,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title: '第六编 文革被彻底否定（1976年9月-） 第二十九章 “揭批查”运动',
+        authors: ['新世纪出版社'],
+        page_start: 807,
+        page_end: 848,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第六编 文革被彻底否定（1976年9月-） 第三十章 “彻底摧毁‘四人帮’的帮派体系”',
+        authors: ['新世纪出版社'],
+        page_start: 849,
+        page_end: 892,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title:
+          '第六编 文革被彻底否定（1976年9月-） 第三十一章 文革参加者的晚年',
+        authors: ['新世纪出版社'],
+        page_start: 893,
+        page_end: 908,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title: '参考资料',
+        authors: ['新世纪出版社'],
+        page_start: 909,
+        page_end: 921,
+        dates: [{ year: 2020, month: 4 }],
+      },
+      {
+        title: '后记',
+        authors: ['新世纪出版社'],
+        page_start: 922,
+        page_end: 927,
+        dates: [{ year: 2020, month: 4 }],
+      },
+    ],
+    ocr: { content_thresholds: [0.066, 0.072, 0, 0] },
+    ocr_exceptions: {},
+  },
+  parser_id: 'automation',
+  path: '/archives3/a90fd792-d48c-426b-9b9b-db3192e62522.pdf',
+};
