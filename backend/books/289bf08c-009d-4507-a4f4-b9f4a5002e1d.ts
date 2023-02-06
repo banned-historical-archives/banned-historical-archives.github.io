8ef6bf35ee335e84269e2a1d6507a4c258dab6d1
PirateBook1 <3572336885@protonmail.com>
2023-02-06 18:00:59 +0800

    fix

--- backend/books/289bf08c-009d-4507-a4f4-b9f4a5002e1d.ts
+++ backend/books/289bf08c-009d-4507-a4f4-b9f4a5002e1d.ts
@@ -1,7 +1,7 @@
 export default {
   entity: {
     id: '289bf08c-009d-4507-a4f4-b9f4a5002e1d',
-    name: '上海工人革命造反总司令部斗争纪要',
+    name: '上海工人革命造反总司令部斗争纪要（上海1967年）',
     internal: false,
     official: true,
     type: 'img',
@@ -14,11 +14,32 @@ export default {
     ext: 'jpg',
     articles: [
       {
-        title: '上海工人革命造反总司令部斗争纪要',
+        title: '插图',
         authors: ['《工人造反报》编辑部', '《一月风暴》编辑部 '],
         dates: [{ year: 1967 }],
-        page_start: 1,
-        page_end: 40,
+        page_start: 2,
+        page_end: 5,
+      },
+      {
+        title: '一、安亭风暴',
+        authors: ['《工人造反报》编辑部', '《一月风暴》编辑部 '],
+        dates: [{ year: 1967 }],
+        page_start: 6,
+        page_end: 12,
+      },
+      {
+        title: '二、《解放日报》事件',
+        authors: ['《工人造反报》编辑部', '《一月风暴》编辑部 '],
+        dates: [{ year: 1967 }],
+        page_start: 12,
+        page_end: 18,
+      },
+      {
+        title: '三、一月革命',
+        authors: ['《工人造反报》编辑部', '《一月风暴》编辑部 '],
+        dates: [{ year: 1967 }],
+        page_start: 19,
+        page_end: 38,
       },
     ],
     ocr: { vsplit: 0.4 },
