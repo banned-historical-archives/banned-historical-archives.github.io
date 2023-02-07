0406696f5f190c9c5ecf3dead24abfa00190d3f7
PirateBook1 <PirateBook1@users.noreply.github.com>
2023-02-07 15:21:58 +0800

    [create-pull-request] automated change

---
+++ backend/books/75640fa5-798f-4ba9-9413-4530c3437a28.ts
@@ -0,0 +1,75 @@
+export default {
+  entity: {
+    id: '75640fa5-798f-4ba9-9413-4530c3437a28',
+    name: '无产阶级文化大革命概述简论（简要本）（水陆洲2012年）',
+    internal: false,
+    official: false,
+    type: 'pdf',
+    author: '水陆洲编',
+    files:
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives3/main/75640fa5-798f-4ba9-9413-4530c3437a28.pdf',
+  },
+  parser_option: {
+    page_limits: [],
+    ext: 'pdf',
+    articles: [
+      {
+        title: '前言',
+        authors: ['水陆洲'],
+        page_start: 1,
+        page_end: 11,
+        dates: [{ year: 2012, month: 9, day: 2 }],
+      },
+      {
+        title: '第一篇 无产阶级专政下继续革命的理论和中国共产党的实际',
+        authors: ['水陆洲'],
+        page_start: 11,
+        page_end: 16,
+        dates: [{ year: 2012, month: 9, day: 2 }],
+      },
+      {
+        title: '第二篇 粉碎以刘少奇头子的资产阶级司令部',
+        authors: ['水陆洲'],
+        page_start: 16,
+        page_end: 25,
+        dates: [{ year: 2012, month: 9, day: 2 }],
+      },
+      {
+        title: '第三篇 文化大革命第二战役的伟大胜利',
+        authors: ['水陆洲'],
+        page_start: 25,
+        page_end: 93,
+        dates: [{ year: 2012, month: 9, day: 2 }],
+      },
+      {
+        title: '第四篇 文化大革命第三战役的伟大胜利',
+        authors: ['水陆洲'],
+        page_start: 94,
+        page_end: 168,
+        dates: [{ year: 2012, month: 9, day: 2 }],
+      },
+      {
+        title: '第五篇 文化大革命被结束',
+        authors: ['水陆洲'],
+        page_start: 168,
+        page_end: 196,
+        dates: [{ year: 2012, month: 9, day: 2 }],
+      },
+      {
+        title: '第六篇 总结经验教训 开辟革命征程',
+        authors: ['水陆洲'],
+        page_start: 197,
+        page_end: 257,
+        dates: [{ year: 2012, month: 9, day: 2 }],
+      },
+    ],
+    ocr: {
+      standard_paragraph_merge_strategy_threshold: 0.186,
+      differential_paragraph_merge_strategy_threshold: 0,
+      content_thresholds: [0.054, 0, 0, 0],
+    },
+    ocr_exceptions: {},
+  },
+  parser_id: 'automation',
+  path: '/archives3/75640fa5-798f-4ba9-9413-4530c3437a28.pdf',
+};
