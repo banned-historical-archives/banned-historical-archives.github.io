0dd711ff3be1d7eccf7f5fe3b9fe29a06a6b87e0
banned-historical-archives <banned-historical-archives@users.noreply.github.com>
2023-02-19 19:43:40 +0800

    [create-pull-request] automated change

---
+++ backend/books/5d509712-5e40-485d-8fdd-1cbd346035c4.ts
@@ -0,0 +1,29 @@
+export default {
+  entity: {
+    id: '5d509712-5e40-485d-8fdd-1cbd346035c4',
+    name: 'test4',
+    internal: false,
+    official: false,
+    type: 'pdf',
+    author: '',
+    files:
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives3/main/5d509712-5e40-485d-8fdd-1cbd346035c4.pdf',
+  },
+  parser_option: {
+    page_limits: [],
+    ext: 'pdf',
+    articles: [
+      {
+        title: '目asdf录',
+        authors: ['毛泽东', '江青'],
+        page_start: 1,
+        page_end: 7,
+        dates: [{ year: 1949, month: 10, day: 1 }],
+      },
+    ],
+    ocr: undefined,
+    ocr_exceptions: {},
+  },
+  parser_id: 'automation',
+  path: '/archives3/5d509712-5e40-485d-8fdd-1cbd346035c4.pdf',
+};
