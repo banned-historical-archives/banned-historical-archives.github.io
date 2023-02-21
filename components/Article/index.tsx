45eb4df97c4bdd569f2f1d29d46ed7a71da8699f
PirateBook1 <113572850+PirateBook1@users.noreply.github.com>
2023-02-21 12:29:14 +0800

    fix quotation style

--- components/Article/index.tsx
+++ components/Article/index.tsx
@@ -183,7 +183,7 @@ function PureArticle({
             .split('\n')
             .filter((j) => j)
             .map((j, j_idx) => (
-              <Typography variant="body1" key={j_idx} sx={{ color: 'grey' }}>
+              <Typography variant="body1" key={j_idx} sx={{ color: 'grey', padding: '0.625em 2.5em 0.625em 2.5em', borderLeft: '2px solid' }}>
                 {j}
               </Typography>
             ))}
