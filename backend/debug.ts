e3b414e40c81100093d8a7bd38116f3be2e54320
Your Name <youremail@yourdomain.com>
2023-02-21 23:39:22 +0800

    rename

--- backend/debug.ts
+++ backend/debug.ts
@@ -14,139 +14,14 @@ import fs from 'fs';
 import { JSDOM } from 'jsdom';
 import { ContentType, Date, ParserResult } from '../types';
 
-function bydir(dir: string, author: string, out: string) {
-  const files = fs.readdirSync(dir);
-  const res = [];
-  for (const file of files) {
-    const a = fs.readFileSync(dir + '/' + file).toString();
-    const dom = new JSDOM(a);
-
-    let title_raw: string[] = [];
-    try {
-      title_raw = dom.window.document
-        .querySelector('.titles')!
-        .innerHTML!.split('<br>')
-        .map((i) => i.trim())
-        .filter((i) => i);
-    } catch {
-      console.log(file);
-      continue;
-    }
-    const dates: Date[] = [];
-    if (/\d+(\.\d+(\.\d+))$/.test(title_raw[title_raw.length - 1])) {
-      const date = Array.from(
-        title_raw[title_raw.length - 1].matchAll(/\d+(\.\d+(\.\d+))$/g),
-      )[0][0];
-      dates.push({
-        year: parseInt(date.split('.')[0]),
-        month: parseInt(date.split('.')[1]),
-        day: parseInt(date.split('.')[2]),
-      });
-      title_raw.pop();
-    }
-    const authors: string[] = author
-      ? [author]
-      : title_raw
-          .slice(1)
-          .reduce((m: string[], i) => [...m, ...i.split(' ')], []);
-    const temp = a.split('</font>')[1];
-    const source = temp?.split('</div>')[0].trim() || '';
-    const p: ParserResult = {
-      title: title_raw[0],
-      authors,
-      dates,
-      is_range_date: false,
-      comment_pivots: [],
-      comments: [],
-      page_start: 0,
-      page_end: 0,
-      description: source,
-      parts: [
-        {
-          type: ContentType.title,
-          text: title_raw[0],
-        },
-      ],
-    };
-    dom.window.document
-      .querySelectorAll(
-        '.contents,.titleR,.headingB,.headingN,.titleB,.inscriberR',
-      )
-      .forEach((i) => {
-        p.parts.push({
-          type:
-            i.className === 'titleB' || i.className === 'headingN'
-              ? ContentType.subtitle
-              : ContentType.paragraph,
-          text: i.textContent!.trim(),
-        });
-      });
-    res.push(p);
-  }
-  fs.writeFileSync(
-    join(__dirname, `parser/GPCR_v3/${out}`),
-    JSON.stringify(res),
-  );
-}
-
-function parse(dir: string, out: string) {
-  const files = fs.readdirSync(dir);
-  const res = [];
-  for (const file of files) {
-    let a = fs
-      .readFileSync(dir + '/' + file)
-      .toString()
-      .split('\r\n');
-    const title = a[0];
-    const authors = a[1].split(' ').filter((i) => i);
-
-    const dates: Date[] = [];
-    if (/\d+(\.\d+(\.\d+))$/.test(a[2])) {
-      const date = Array.from(a[2].matchAll(/\d+(\.\d+(\.\d+))$/g))[0][0];
-      dates.push({
-        year: parseInt(date.split('.')[0]),
-        month: parseInt(date.split('.')[1]),
-        day: parseInt(date.split('.')[2]),
-      });
-    }
-    const source = /^来源：/.test(a[a.length - 1]) ? a[a.length - 1] : '';
-    const p: ParserResult = {
-      title,
-      authors,
-      dates,
-      is_range_date: false,
-      comment_pivots: [],
-      comments: [],
-      page_start: 0,
-      page_end: 0,
-      description: source,
-      parts: [
-        {
-          type: ContentType.title,
-          text: title,
-        },
-      ],
-    };
-    a.slice(3).forEach((i) => {
-      if (i)
-        p.parts.push({
-          type: ContentType.paragraph,
-          text: i,
-        });
-    });
-    res.push(p);
-  }
-  fs.writeFileSync(
-    join(__dirname, `parser/GPCR_v3/${out}`),
-    JSON.stringify(res),
-  );
-}
-
 (async () => {
   const ds = await init();
   const books = await get_books();
-  const book = books.find((i) => i.entity.id === 'GPCRv3')!;
-  console.log(await book.parser(book.path, book.parser_option));
+  const book = books.find((i) => i.entity.id === '4487f711-d1dc-4c33-a1a9-50a1b7f7d659')!;
+
+
+  console.log(get_article_id((await book.parser(book.path, book.parser_option))[0]))
+  // console.log(await book.parser(book.path, book.parser_option));
 
   debugger;
   // parse(join(__dirname, '../a'), 'zs.ts');
