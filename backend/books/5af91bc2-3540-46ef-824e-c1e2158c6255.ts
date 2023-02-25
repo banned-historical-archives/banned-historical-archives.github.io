8b4d1ac0690a6bf82b10828d194ff26b76d2f7b9
PirateBook1 <PirateBook1@users.noreply.github.com>
2023-02-25 20:51:07 +0800

    [create-pull-request] automated change

--- backend/books/5af91bc2-3540-46ef-824e-c1e2158c6255.ts
+++ backend/books/5af91bc2-3540-46ef-824e-c1e2158c6255.ts
@@ -15,33 +15,43 @@ export default {
     ext: 'jpg',
     articles: [
       {
-        title: '张春桥接见红卫兵上海市大专院校革命委员会及上海市工人革命造反总司令部代表的谈话',
-        alias: '张春桥同志接见上海“红革会”与“工人革命造反总司令部”代表的谈话记录',
+        title:
+          '张春桥接见红卫兵上海市大专院校革命委员会及上海市工人革命造反总司令部代表的谈话',
+        alias:
+          '张春桥同志接见上海“红革会”与“工人革命造反总司令部”代表的谈话记录',
         authors: ['张春桥'],
         page_start: 1,
         page_end: 8,
-        tags: [{
-          name: '对话',
-          type: 'articleType'
-        }, {
-          name: '张春桥',
-          type: 'character'
-        }, {
-          name: '红卫兵上海市大专院校革命委员会',
-          type: 'character'
-        }, {
-          name: '上海市工人革命造反总司令部',
-          type: 'character'
-        }, {
-          name: '中央音乐学院红教工“劲松”战斗组',
-          type: 'reprint'
-        }, {
-          name: '上海文艺界革命造反派纵队上海交响乐团支队',
-          type: 'reprint'
-        }, {
-          name: '上海革命文工团“红旗”、“鲁迅”战斗队',
-          type: 'reprint'
-        }],
+        tags: [
+          {
+            name: '对话',
+            type: 'articleType',
+          },
+          {
+            name: '张春桥',
+            type: 'character',
+          },
+          {
+            name: '红卫兵上海市大专院校革命委员会',
+            type: 'character',
+          },
+          {
+            name: '上海市工人革命造反总司令部',
+            type: 'character',
+          },
+          {
+            name: '中央音乐学院红教工“劲松”战斗组',
+            type: 'reprint',
+          },
+          {
+            name: '上海文艺界革命造反派纵队上海交响乐团支队',
+            type: 'reprint',
+          },
+          {
+            name: '上海革命文工团“红旗”、“鲁迅”战斗队',
+            type: 'reprint',
+          },
+        ],
         dates: [{ year: 1966, month: 12, day: 23 }],
       },
     ],
--- backend/books/67b43734-d924-48ac-9e62-359edd11e690.ts
+++ backend/books/67b43734-d924-48ac-9e62-359edd11e690.ts
@@ -5,7 +5,8 @@ export default {
     internal: false,
     official: false,
     type: 'img',
-    author: '康生，江青、姚文元，1967.9.5，安徽无产阶级革命派（P 派）赴京汇报代表团整理',
+    author:
+      '康生，江青、姚文元，1967.9.5，安徽无产阶级革命派（P 派）赴京汇报代表团整理',
     files:
       'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/67b43734-d924-48ac-9e62-359edd11e690/1.jpg,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/67b43734-d924-48ac-9e62-359edd11e690/2.jpg,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/67b43734-d924-48ac-9e62-359edd11e690/3.jpg,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/67b43734-d924-48ac-9e62-359edd11e690/4.jpg,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/67b43734-d924-48ac-9e62-359edd11e690/5.jpg,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/67b43734-d924-48ac-9e62-359edd11e690/6.jpg,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/67b43734-d924-48ac-9e62-359edd11e690/7.jpg,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/67b43734-d924-48ac-9e62-359edd11e690/8.jpg',
   },
@@ -19,28 +20,36 @@ export default {
         authors: ['康生', '江青', '姚文元'],
         page_start: 1,
         page_end: 8,
-        tags: [{
-          name: '指示',
-          type: 'articleType'
-        }, {
-          name: '康生',
-          type: 'character'
-        }, {
-          name: '江青',
-          type: 'character'
-        }, {
-          name: '姚文元',
-          type: 'character'
-        }, {
-          name: '安徽无产阶级革命派（P派）',
-          type: 'character'
-        }, {
-          name: '安徽文革',
-          type: 'subject'
-        }, {
-          name: '安徽无产阶级革命派（P派）',
-          type: 'reprint'
-        }],
+        tags: [
+          {
+            name: '指示',
+            type: 'articleType',
+          },
+          {
+            name: '康生',
+            type: 'character',
+          },
+          {
+            name: '江青',
+            type: 'character',
+          },
+          {
+            name: '姚文元',
+            type: 'character',
+          },
+          {
+            name: '安徽无产阶级革命派（P派）',
+            type: 'character',
+          },
+          {
+            name: '安徽文革',
+            type: 'subject',
+          },
+          {
+            name: '安徽无产阶级革命派（P派）',
+            type: 'reprint',
+          },
+        ],
         dates: [{ year: 1967, month: 9, day: 5 }],
       },
     ],
