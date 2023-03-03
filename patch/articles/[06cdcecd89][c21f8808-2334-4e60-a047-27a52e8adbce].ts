cff9e53ba77c93584c2d7a66ba142397606d05a8
PirateBook1 <PirateBook1@users.noreply.github.com>
2023-03-03 12:41:26 +0000

    [create-pull-request] automated change

--- patch/articles/[06cdcecd89][c21f8808-2334-4e60-a047-27a52e8adbce].ts
+++ patch/articles/[06cdcecd89][c21f8808-2334-4e60-a047-27a52e8adbce].ts
@@ -2,4 +2,6 @@
 export default [
 // {"version":2,"parts":{"0":{"delete":true},"1":{"delete":true},"2":{"delete":true},"3":{"type":"title"},"4":{"diff":"=92\t-1\t+厉\t=78\t+赵\t=15\t-1\t+赵\t=16\t-1\t+搞\t=38\t-1\t+搞\t=30\t+辫\t=38\t-31"},"5":{"delete":true},"6":{"delete":true},"7":{"delete":true}},"comments":{},"description":""}
   {"version":2,"parts":{"0":{"delete":true},"1":{"delete":true},"2":{"delete":true},"3":{"type":"title"},"4":{"diff":"=92\t-1\t+%E5%8E%89\t=78\t+%E8%B5%B5\t=15\t-1\t+%E8%B5%B5\t=16\t-1\t+%E6%90%9E\t=38\t-1\t+%E6%90%9E\t=30\t+%E8%BE%AB\t=38\t-31"},"5":{"delete":true},"6":{"delete":true},"7":{"delete":true}},"comments":{},"description":""},
+// {"version":2,"parts":{},"comments":{},"description":"+中共武汉市委材料组，一九七六年十二月"}
+  {"version":2,"parts":{},"comments":{},"description":"+%E4%B8%AD%E5%85%B1%E6%AD%A6%E6%B1%89%E5%B8%82%E5%A7%94%E6%9D%90%E6%96%99%E7%BB%84%EF%BC%8C%E4%B8%80%E4%B9%9D%E4%B8%83%E5%85%AD%E5%B9%B4%E5%8D%81%E4%BA%8C%E6%9C%88"},
 ];
