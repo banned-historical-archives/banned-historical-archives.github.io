e3266fd1c152be0d95692699b0dc8a50c5dc5f7b
Worker <113572850+PirateBook1@users.noreply.github.com>
2023-03-08 11:59:33 +0800

    fix 7

--- backend/books/2cec027f-70be-4525-8a87-8da2e7cfa6fc.ts
+++ backend/books/2cec027f-70be-4525-8a87-8da2e7cfa6fc.ts
@@ -1,11 +1,11 @@
 export default {
   entity: {
     id: '2cec027f-70be-4525-8a87-8da2e7cfa6fc',
-    name: '毛远新在省委常委会议上的发言（摘录）',
-    internal: true,
+    name: '毛远新在省委常委会议上的发言（1976.12）',
+    internal: false,
     official: true,
     type: 'img',
-    author: '',
+    author: '毛远新，1976.12，反攻倒算材料',
     files:
       'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/2cec027f-70be-4525-8a87-8da2e7cfa6fc/1.png,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/2cec027f-70be-4525-8a87-8da2e7cfa6fc/2.png,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/2cec027f-70be-4525-8a87-8da2e7cfa6fc/3.png,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/2cec027f-70be-4525-8a87-8da2e7cfa6fc/4.png',
   },
@@ -15,6 +15,7 @@ export default {
     articles: [
       {
         title: '毛远新在省委常委会议上的发言',
+        alias: '毛远新一九七四年二月九日在省委常委会议上的发言（摘录）',
         authors: ['毛远新'],
         page_start: 2,
         page_end: 4,
--- backend/books/5c2a93f5-a845-42ca-aeb7-69a6f92fc954.ts
+++ backend/books/5c2a93f5-a845-42ca-aeb7-69a6f92fc954.ts
@@ -1,11 +1,11 @@
 export default {
   entity: {
     id: '5c2a93f5-a845-42ca-aeb7-69a6f92fc954',
-    name: '毛远新在市地委书记会议召集人会上的讲话记录',
-    internal: true,
+    name: '毛远新在市地委书记会议召集人会上的讲话记录（1976.11）',
+    internal: false,
     official: true,
     type: 'img',
-    author: '',
+    author: '毛远新，1976.11，反攻倒算材料，大东区无线电器材厂',
     files:
       'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/5c2a93f5-a845-42ca-aeb7-69a6f92fc954/1.jpg,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/5c2a93f5-a845-42ca-aeb7-69a6f92fc954/2.jpg,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/5c2a93f5-a845-42ca-aeb7-69a6f92fc954/3.jpg,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/5c2a93f5-a845-42ca-aeb7-69a6f92fc954/4.jpg,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/5c2a93f5-a845-42ca-aeb7-69a6f92fc954/5.jpg,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/5c2a93f5-a845-42ca-aeb7-69a6f92fc954/6.jpg,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/5c2a93f5-a845-42ca-aeb7-69a6f92fc954/7.jpg',
   },
