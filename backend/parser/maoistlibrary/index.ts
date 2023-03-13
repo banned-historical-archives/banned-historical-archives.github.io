9ddac80263f2fad864a73023c12c110ca14e8719
Worker <113572850+PirateBook1@users.noreply.github.com>
2023-03-13 12:18:00 +0800

    add philippines.ts

--- backend/parser/maoistlibrary/index.ts
+++ backend/parser/maoistlibrary/index.ts
@@ -14,6 +14,7 @@ import { normalize } from '../../utils';
 import { join, basename } from 'node:path/posix';
 import peru from './peru';
 import india from './india';
+import philippines from './philippines';
 
 export async function parse(
   path: string,
@@ -22,5 +23,6 @@ export async function parse(
   return [
     ...peru,
     ...india,
+    ...philippines,
   ] as ParserResult[];
 }
