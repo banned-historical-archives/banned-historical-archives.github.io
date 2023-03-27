baf8bbb143b0fb8107bde364bf68d3cb11521fd6
Your Name <youremail@yourdomain.com>
2023-03-27 19:36:46 +0800

    fix

--- backend/build-readme.ts
+++ backend/build-readme.ts
@@ -26,138 +26,154 @@ import { normalize } from './utils';
 import fs from 'fs';
 
 class Line {
-    [x: string]: any;
-    constructor(id: any, title: any, author: any, internal: any, official: any) {
-        this.id = id
-        this.title = title
-        this.author = author
-        this.internal = internal
-        this.official = official
-        this.status = '未知'
-        this.all = 0
-    }
+  [x: string]: any;
+  constructor(id: any, title: any, author: any, internal: any, official: any) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.internal = internal;
+    this.official = official;
+    this.status = '未知';
+    this.all = 0;
+  }
 }
 
-const temp: any[] = fs.readdirSync(join(__dirname, './books'), { encoding: 'utf-8' })
+const temp: any[] = fs.readdirSync(join(__dirname, './books'), {
+  encoding: 'utf-8',
+});
 
-let judged: any[] = fs.readdirSync(join(__dirname, '../patch/articles'), { encoding: 'utf-8' })
+let judged: any[] = fs.readdirSync(join(__dirname, '../patch/articles'), {
+  encoding: 'utf-8',
+});
 
-const map = new Map()
+const map = new Map();
 
 temp.forEach((item: string) => {
-    const name = item.replace('.ts', '.js')
-    const data = fs.readFileSync(join(__dirname, './books', item), { encoding: 'utf-8' })
-    if (!fs.existsSync(join(__dirname, './temp'))) {
-        fs.mkdirSync(join(__dirname, './temp'))
-    }
-    fs.writeFileSync(join(__dirname, './temp', name), data)
-})
+  const name = item.replace('.ts', '.js');
+  const data = fs.readFileSync(join(__dirname, './books', item), {
+    encoding: 'utf-8',
+  });
+  if (!fs.existsSync(join(__dirname, './temp'))) {
+    fs.mkdirSync(join(__dirname, './temp'));
+  }
+  fs.writeFileSync(join(__dirname, './temp', name), data);
+});
 
-const objs: any[] = []
+const objs: any[] = [];
 
 const dirPath = join(__dirname, '../README.md');
 const data = fs.readFileSync(dirPath, { encoding: 'utf-8' });
-const mid1 = '| ----- | --------------- | -------------- | -------------- | --------- |'
-const mid2 = `以及许多未知来源的笔记/讲话记录/文稿/手稿/翻印稿正在录入。`
+const mid1 =
+  '| ----- | --------------- | -------------- | -------------- | --------- |';
+const mid2 = `以及许多未知来源的笔记/讲话记录/文稿/手稿/翻印稿正在录入。`;
 
-const first = data.split(mid1)[0]
-const last = data.split(mid2)[1]
+const first = data.split(mid1)[0];
+const last = data.split(mid2)[1];
 
 init()
-    .then(async (AppDataSource) => {
-        try {
-            const repository = await AppDataSource.getRepository(Publication);
-            const all_publications = await repository.find();
+  .then(async (AppDataSource) => {
+    try {
+      const repository = await AppDataSource.getRepository(Publication);
+      const all_publications = await repository.find();
 
-            all_publications.forEach(item => {
-                let internal = item.internal ? 'Y' : 'N'
-                let official = item.official ? 'Y' : 'N'
-                objs.push(new Line(item.id, item.name, item.author, internal, official))
-            })
+      all_publications.forEach((item) => {
+        let internal = item.internal ? 'Y' : 'N';
+        let official = item.official ? 'Y' : 'N';
+        objs.push(
+          new Line(item.id, item.name, item.author, internal, official),
+        );
+      });
 
-            objs.sort((a, b) => {
-                if (a.title.length < b.title.length) {
-                    return -1
-                } else if (a.title.length === b.title.length) {
-                    return a.title.localeCompare(b.title)
-                } else {
-                    return 1
-                }
-            })
-        } catch (error) {
-            throw new Error(`ERROR: ${error}`);
+      objs.sort((a, b) => {
+        if (a.title.length < b.title.length) {
+          return -1;
+        } else if (a.title.length === b.title.length) {
+          return a.title.localeCompare(b.title);
+        } else {
+          return 1;
         }
-    })
-    .then(async () => {
-        for (let i = 0; i < temp.length; i++) {
-            const article = await import(join(__dirname, './books/', temp[i]))
-            temp[i] = article.default
+      });
+    } catch (error) {
+      throw new Error(`ERROR: ${error}`);
+    }
+  })
+  .then(async () => {
+    for (let i = 0; i < temp.length; i++) {
+      const article = await import(join(__dirname, './books/', temp[i]));
+      temp[i] = article.default;
+    }
+  })
+  .then(() => {
+    judged.forEach((item, i) => {
+      item = item.replace('.ts', '');
+      item = item.split('');
+      item.shift();
+      item.pop();
+      item = item.join('');
+      let temp = item.split('][');
+      judged[i] = temp[1] + '+' + temp[0];
+    });
+
+    const set = new Set(judged);
+    judged = Array.from(set);
+
+    judged.forEach((item, i) => {
+      let arr = item.split('+');
+      judged[i] = {
+        id: arr[0],
+        articleId: arr[1],
+      };
+    });
+    judged.forEach((item) => {
+      if (map.has(item.id)) {
+        let value = map.get(item.id);
+        map.set(item.id, value + 1);
+      } else {
+        map.set(item.id, 1);
+      }
+    });
+  })
+  .then(() => {
+    let status: any;
+    temp.forEach((item) => {
+      try {
+        if (item.parser_option.articles.length === map.get(item.entity.id)) {
+          status = '已校对';
+        } else if (
+          item.parser_option.articles.length !== map.get(item.entity.id) &&
+          map.get(item.entity.id) > 0
+        ) {
+          status = '正在校对';
+        } else {
+          status = '未校对';
         }
-    })
-    .then(() => {
-        judged.forEach((item, i) => {
-            item = item.replace('.ts', '')
-            item = item.split('')
-            item.shift()
-            item.pop()
-            item = item.join('')
-            let temp = item.split('][')
-            judged[i] = temp[1] + '+' + temp[0]
-        })
-
-        const set = new Set(judged)
-        judged = Array.from(set)
-
-        judged.forEach((item, i) => {
-            let arr = item.split('+')
-            judged[i] = {
-                id: arr[0],
-                articleId: arr[1]
-            }
-        })
-        judged.forEach(item => {
-            if (map.has(item.id)) {
-                let value = map.get(item.id)
-                map.set(item.id, value + 1)
-            } else {
-                map.set(item.id, 1)
-            }
-        })
-    })
-    .then(() => {
-        let status: any
-        temp.forEach(item => {
-            try {
-                if (item.parser_option.articles.length === map.get(item.entity.id)) {
-                    status = '已校对'
-                } else if (item.parser_option.articles.length !== map.get(item.entity.id) && map.get(item.entity.id) > 0) {
-                    status = '正在校对'
-                } else {
-                    status = '未校对'
-                }
-            } catch(err) {
-                status = '未知'
-            } finally {
-                for (let i = 0; i < objs.length; i++) {
-                    if (objs[i].id === item.entity.id) {
-                        objs[i].status = status
-                        break
-                    }
-                }
-            }
-        })
-        objs.forEach((item, i) => {
-            objs[i] = `${ item.title }|${ item.author }|${ item.internal }|${ item.official }|${ item.status }`
-        })
-        const result = first + mid1 + '\n' + objs.join('\n') + '\n\n' + mid2 + last;
-        fs.writeFileSync(dirPath, result)
-    })
-    .then(() => {
-        const arr = fs.readdirSync(join(__dirname, './temp'), { encoding: 'utf-8' })
-        arr.forEach(item => {
-            const name = item.replace('.ts', '.js')
-            fs.rmSync(join(__dirname, './temp', name))
-        })
-        process.exit()
-    })
-    .catch((error) => console.log(error));
+      } catch (err) {
+        status = '未知';
+      } finally {
+        for (let i = 0; i < objs.length; i++) {
+          if (objs[i].id === item.entity.id) {
+            objs[i].status = status;
+            break;
+          }
+        }
+      }
+    });
+    objs.forEach((item, i) => {
+      objs[
+        i
+      ] = `${item.title}|${item.author}|${item.internal}|${item.official}|${item.status}`;
+    });
+    const result = first + mid1 + '\n' + objs.join('\n') + '\n\n' + mid2 + last;
+    fs.writeFileSync(dirPath, result);
+  })
+  .then(() => {
+    const arr = fs.readdirSync(join(__dirname, './temp'), {
+      encoding: 'utf-8',
+    });
+    arr.forEach((item) => {
+      const name = item.replace('.ts', '.js');
+      fs.rmSync(join(__dirname, './temp', name));
+    });
+    process.exit();
+  })
+  .catch((error) => console.log(error));
--- backend/build-txt.ts
+++ backend/build-txt.ts
@@ -26,16 +26,19 @@ import { normalize } from './utils';
 import fs from 'fs';
 import { ensureDirSync } from 'fs-extra';
 
-init()
-.then(async (AppDataSource) => {
-  const data: any[] = []
+init().then(async (AppDataSource) => {
+  const data: any[] = [];
 
   const publication = await AppDataSource.getRepository(Publication);
-  const all_publications = await publication.find({ relations: ["pages"] })
+  const all_publications = await publication.find({ relations: ['pages'] });
 
-  const comment = await AppDataSource.getRepository(Comment).createQueryBuilder()
+  const comment = await AppDataSource.getRepository(
+    Comment,
+  ).createQueryBuilder();
 
-  const content = await AppDataSource.getRepository(Content).createQueryBuilder()
+  const content = await AppDataSource.getRepository(
+    Content,
+  ).createQueryBuilder();
 
   /**
   const temp = {
@@ -61,35 +64,39 @@ init()
   }
   */
 
-  all_publications.forEach(item => {
-    const pages: any[] = []
+  all_publications.forEach((item) => {
+    const pages: any[] = [];
     item.pages.forEach((it, i) => {
-        pages[i] = {
-            articleId: it.articleId,
-        }
-    })
+      pages[i] = {
+        articleId: it.articleId,
+      };
+    });
     data.push({
-        id: item.id,
-        pages: pages,
-    })
-  })
+      id: item.id,
+      pages: pages,
+    });
+  });
 
   try {
     // 拿注释
     for (let i = 0; i < data.length; i++) {
       for (let j = 0; j < data[i].pages.length; j++) {
-          // articleId = ? AND (`index` != -1 OR `part_index` != -1 OR `offset` != -1) order by `index`
-          let obj = await comment
-              .where('articleId = :articleId', { articleId: data[i].pages[j].articleId })
-              .andWhere(new Brackets(qb => {
-                  qb.where('`index` != :index', { index: -1 })
-                      .orWhere('part_index != :part_index', { part_index: -1 })
-                      .orWhere('`offset` != :offset', { offset: -1 })
-              }))
-              .orderBy('`index`', 'ASC')
-              .getMany()
-          const [...temp1] = obj
-          data[i].pages[j].comment = temp1
+        // articleId = ? AND (`index` != -1 OR `part_index` != -1 OR `offset` != -1) order by `index`
+        let obj = await comment
+          .where('articleId = :articleId', {
+            articleId: data[i].pages[j].articleId,
+          })
+          .andWhere(
+            new Brackets((qb) => {
+              qb.where('`index` != :index', { index: -1 })
+                .orWhere('part_index != :part_index', { part_index: -1 })
+                .orWhere('`offset` != :offset', { offset: -1 });
+            }),
+          )
+          .orderBy('`index`', 'ASC')
+          .getMany();
+        const [...temp1] = obj;
+        data[i].pages[j].comment = temp1;
       }
     }
 
@@ -98,47 +105,55 @@ init()
       for (let j = 0; j < data[i].pages.length; j++) {
         // articleId = ? AND publicationID = ? order by `index`
         let obj = await content
-            .where('articleId = :articleId', { articleId: data[i].pages[j].articleId })
-            .andWhere('publicationID = :publicationID', { publicationID: data[i].id })
-            .orderBy('`index`', 'ASC')
-            .getMany()
-        data[i].pages[j].content = new Array()
-        obj.forEach(item => {
+          .where('articleId = :articleId', {
+            articleId: data[i].pages[j].articleId,
+          })
+          .andWhere('publicationID = :publicationID', {
+            publicationID: data[i].id,
+          })
+          .orderBy('`index`', 'ASC')
+          .getMany();
+        data[i].pages[j].content = new Array();
+        obj.forEach((item) => {
           data[i].pages[j].content.push({
             type: item.type,
             text: item.text,
-          })
-        })
+          });
+        });
 
-          // 处理正文内注释符
+        // 处理正文内注释符
         if (data[i].pages[j].comment) {
           for (let m = data[i].pages[j].comment.length - 1; m >= 0; m--) {
-            let index = data[i].pages[j].comment[m].index
-            let part_index = data[i].pages[j].comment[m].part_index
-            let offset = data[i].pages[j].comment[m].offset
+            let index = data[i].pages[j].comment[m].index;
+            let part_index = data[i].pages[j].comment[m].part_index;
+            let offset = data[i].pages[j].comment[m].offset;
             if (part_index === -99 && offset === -99) {
-              continue
+              continue;
             } else {
               try {
                 if (data[i].pages[j].content[part_index].text !== undefined) {
-                  let str = data[i].pages[j].content[part_index].text
-                  str = str.split('')
-                  str.splice(offset, 0, `〔${ index }〕`)
-                  str = str.join('')
-                  data[i].pages[j].content[part_index].text = str
+                  let str = data[i].pages[j].content[part_index].text;
+                  str = str.split('');
+                  str.splice(offset, 0, `〔${index}〕`);
+                  str = str.join('');
+                  data[i].pages[j].content[part_index].text = str;
                 } else {
-                  data[i].pages[j].content[part_index].text = ''
+                  data[i].pages[j].content[part_index].text = '';
                 }
               } catch (err) {
-                console.log('处理失败！文章 id：' + data[i].pages[j].articleId + '等待校对！')
+                console.log(
+                  '处理失败！文章 id：' +
+                    data[i].pages[j].articleId +
+                    '等待校对！',
+                );
               }
             }
           }
         }
       }
     }
-  } catch(err) {
-    console.log(err)
+  } catch (err) {
+    console.log(err);
   }
 
   console.log('init');
@@ -157,19 +172,25 @@ init()
       const id = get_article_id(i);
       ensureDirSync(dirPath);
 
-      let content_arr: any[] = []
-      data.forEach(item => {
+      let content_arr: any[] = [];
+      data.forEach((item) => {
         if (item.id === book.entity.id) {
-          item.pages.forEach((it: { articleId: string; content: any[]; }) => {
+          item.pages.forEach((it: { articleId: string; content: any[] }) => {
             if (it.articleId === id) {
-              [...content_arr] = it.content
+              [...content_arr] = it.content;
             }
-          })
+          });
         }
-      })
+      });
 
       fs.writeFileSync(
-        join(dirPath, `${i.title.replace(/\//g, '').replace(/\*/g, '').substring(0, 50)}[${id}]`),
+        join(
+          dirPath,
+          `${i.title
+            .replace(/\//g, '')
+            .replace(/\*/g, '')
+            .substring(0, 50)}[${id}]`,
+        ),
         `id: ${id}
 标题：${i.title}
 日期：${i.dates
@@ -183,14 +204,16 @@ init()
 书籍作者：${book.entity.author}
 
 正文：
-${content_arr.map((j) => {
-  if (j.type === 'title') return (`# ${ j.text }`)
-  else if (j.type === 'subtitle') return (`## ${ j.text }`)
-  else if (j.type === 'subtitle2') return (`### ${ j.text }`)
-  else if (j.type === 'subtitle3') return (`#### ${ j.text }`)
-  else if (j.type === 'quotation') return (`> ${ j.text }`)
-  else return j.text
-}).join('\n\n')}
+${content_arr
+  .map((j) => {
+    if (j.type === 'title') return `# ${j.text}`;
+    else if (j.type === 'subtitle') return `## ${j.text}`;
+    else if (j.type === 'subtitle2') return `### ${j.text}`;
+    else if (j.type === 'subtitle3') return `#### ${j.text}`;
+    else if (j.type === 'quotation') return `> ${j.text}`;
+    else return j.text;
+  })
+  .join('\n\n')}
 
 描述：
 ${i.description || ''}
@@ -204,4 +227,4 @@ ${i.comments.map((j, idx) => `〔${idx + 1}〕 ${j}`).join('\n\n')}
   }
   console.log('done');
   process.exit();
-})
+});
