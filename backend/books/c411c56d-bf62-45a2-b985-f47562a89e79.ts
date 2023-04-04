f9039def9498c4ea381315276a23ab691b62dcad
banned-historical-archives <banned-historical-archives@users.noreply.github.com>
2023-04-05 02:24:57 +0800

    [create-pull-request] automated change

---
+++ backend/books/c411c56d-bf62-45a2-b985-f47562a89e79.ts
@@ -0,0 +1,29 @@
+export default {
+  entity: {
+    id: 'c411c56d-bf62-45a2-b985-f47562a89e79',
+    name: '毛泽东全集第7卷2',
+    internal: false,
+    official: false,
+    type: 'pdf',
+    author: '',
+    files:
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/c411c56d-bf62-45a2-b985-f47562a89e79.pdf',
+  },
+  parser_option: {
+    page_limits: [],
+    ext: 'pdf',
+    articles: [
+      {
+        title: '在中央政治局会议上的讲话',
+        authors: ['毛泽东', '江青'],
+        page_start: 1,
+        page_end: 294,
+        dates: [{ year: 1949, month: 10, day: 1 }],
+      },
+    ],
+    ocr: undefined,
+    ocr_exceptions: {},
+  },
+  parser_id: 'automation',
+  path: '/archives1/c411c56d-bf62-45a2-b985-f47562a89e79.pdf',
+};
