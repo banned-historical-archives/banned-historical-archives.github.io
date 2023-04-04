ac6221c6c4e90c7f0e0519bd98228f43d3da1286
banned-historical-archives <banned-historical-archives@users.noreply.github.com>
2023-04-05 02:24:11 +0800

    [create-pull-request] automated change

---
+++ backend/books/138716d0-da2a-4eea-a0e5-25d7157e0b60.ts
@@ -0,0 +1,29 @@
+export default {
+  entity: {
+    id: '138716d0-da2a-4eea-a0e5-25d7157e0b60',
+    name: '毛泽东全集第9卷1',
+    internal: false,
+    official: false,
+    type: 'pdf',
+    author: '',
+    files:
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/138716d0-da2a-4eea-a0e5-25d7157e0b60.pdf',
+  },
+  parser_option: {
+    page_limits: [],
+    ext: 'pdf',
+    articles: [
+      {
+        title: '在中央政治局会议上的讲话',
+        authors: ['毛泽东', '江青'],
+        page_start: 1,
+        page_end: 299,
+        dates: [{ year: 1949, month: 10, day: 1 }],
+      },
+    ],
+    ocr: undefined,
+    ocr_exceptions: {},
+  },
+  parser_id: 'automation',
+  path: '/archives1/138716d0-da2a-4eea-a0e5-25d7157e0b60.pdf',
+};
