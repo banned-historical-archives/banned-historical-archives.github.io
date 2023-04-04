ef39fb224fb072d367fc1ede870f502c78da0807
banned-historical-archives <banned-historical-archives@users.noreply.github.com>
2023-04-05 02:19:58 +0800

    [create-pull-request] automated change

---
+++ backend/books/e9ced476-c356-41ea-b89c-ee1592a4f86d.ts
@@ -0,0 +1,29 @@
+export default {
+  entity: {
+    id: 'e9ced476-c356-41ea-b89c-ee1592a4f86d',
+    name: '毛泽东全集第4卷1',
+    internal: false,
+    official: false,
+    type: 'pdf',
+    author: '',
+    files:
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/e9ced476-c356-41ea-b89c-ee1592a4f86d.pdf',
+  },
+  parser_option: {
+    page_limits: [],
+    ext: 'pdf',
+    articles: [
+      {
+        title: '在中央政治局会议上的讲话',
+        authors: ['毛泽东', '江青'],
+        page_start: 1,
+        page_end: 339,
+        dates: [{ year: 1949, month: 10, day: 1 }],
+      },
+    ],
+    ocr: undefined,
+    ocr_exceptions: {},
+  },
+  parser_id: 'automation',
+  path: '/archives1/e9ced476-c356-41ea-b89c-ee1592a4f86d.pdf',
+};
