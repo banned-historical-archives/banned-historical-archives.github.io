df65fd28298f8530285b4bdf3e2fa0201f6f53e0
PirateBook1 <PirateBook1@users.noreply.github.com>
2023-06-27 12:37:05 +0800

    [create-pull-request] automated change

---
+++ backend/books/f7268d77-f359-45df-b7dd-b1c982706aed.ts
@@ -0,0 +1,100 @@
+export default {
+  entity: {
+    id: 'f7268d77-f359-45df-b7dd-b1c982706aed',
+    name: '红旗一九六一年第九、十期',
+    internal: false,
+    official: true,
+    type: 'pdf',
+    author: '《红旗》杂志编辑部',
+    files:
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives2/main/f7268d77-f359-45df-b7dd-b1c982706aed.pdf',
+  },
+  parser_option: {
+    page_limits: [],
+    ext: 'pdf',
+    articles: [
+      {
+        title: '调查研究是马克思主义的根本方法',
+        authors: ['施东向'],
+        page_start: 3,
+        page_end: 12,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 5 }],
+      },
+      {
+        title: '古巴人民抗美爱国斗争胜利的伟大意义',
+        authors: ['于兆力'],
+        page_start: 13,
+        page_end: 19,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 5 }],
+      },
+      {
+        title: '再谈机床内部矛盾运动的规律和机床的“积木化”问题',
+        authors: [
+          '哈尔滨工业大学机械系机床及自动化专业教师',
+          '侯镇冰',
+          '郑明',
+          '关士续',
+          '陆纪培',
+        ],
+        page_start: 20,
+        page_end: 43,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 5 }],
+      },
+      {
+        title: '查明地下水资源 支援农业生产',
+        authors: ['何长工'],
+        page_start: 44,
+        page_end: 49,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 5 }],
+      },
+      {
+        title: '加速发展化学肥料和农药的生产',
+        authors: ['李超伯'],
+        page_start: 50,
+        page_end: 54,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 5 }],
+      },
+      {
+        title: '充分利用肥源，增积有机肥料',
+        authors: ['土丰'],
+        page_start: 55,
+        page_end: 59,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 5 }],
+      },
+    ],
+    ocr: { content_thresholds: [0, 0.017, 0, 0.2] },
+    ocr_exceptions: {},
+  },
+  parser_id: 'automation',
+  path: '/archives2/f7268d77-f359-45df-b7dd-b1c982706aed.pdf',
+};
