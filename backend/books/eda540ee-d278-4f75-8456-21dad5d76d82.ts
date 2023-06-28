1ab21ba901bf524ec54dcca6aa0b79ec46e88fe2
PirateBook1 <PirateBook1@users.noreply.github.com>
2023-06-28 10:17:44 +0800

    [create-pull-request] automated change

---
+++ backend/books/eda540ee-d278-4f75-8456-21dad5d76d82.ts
@@ -0,0 +1,106 @@
+export default {
+  entity: {
+    id: 'eda540ee-d278-4f75-8456-21dad5d76d82',
+    name: '红旗一九六一年第十五、六期',
+    internal: false,
+    official: true,
+    type: 'pdf',
+    author: '《红旗》杂志编辑部',
+    files:
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives2/main/eda540ee-d278-4f75-8456-21dad5d76d82.pdf',
+  },
+  parser_option: {
+    page_limits: [],
+    ext: 'pdf',
+    articles: [
+      {
+        title: '自然科学与世界观',
+        authors: ['龚育之'],
+        page_start: 3,
+        page_end: 18,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 8 }],
+      },
+      {
+        title: '认真扶植新生事物',
+        authors: ['甘子玉'],
+        page_start: 19,
+        page_end: 23,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 8 }],
+      },
+      {
+        title: '关于学术问题的自由讨论',
+        authors: ['刘际启', '周璞'],
+        page_start: 24,
+        page_end: 28,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 8 }],
+      },
+      {
+        title: '论现阶段农村人民公社的根本制度',
+        authors: ['许涤新'],
+        page_start: 29,
+        page_end: 40,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 8 }],
+      },
+      {
+        title: '因地制宜地实行“三包一奖”制度',
+        authors: ['舒代新', '富文业'],
+        page_start: 41,
+        page_end: 47,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 8 }],
+      },
+      {
+        title: '把矿山工作放在冶金工业的首位',
+        authors: ['高扬文'],
+        page_start: 48,
+        page_end: 54,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 8 }],
+      },
+      {
+        title: '肯尼迪岂能扭转历史潮流',
+        authors: ['郭济洲'],
+        page_start: 55,
+        page_end: 59,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 8 }],
+      },
+    ],
+    ocr: { content_thresholds: [0, 0.017, 0, 0.2] },
+    ocr_exceptions: {},
+  },
+  parser_id: 'automation',
+  path: '/archives2/eda540ee-d278-4f75-8456-21dad5d76d82.pdf',
+};
