57f7c8d2f3c6eaa30783509fde497f98ef477b96
PirateBook1 <113572850+PirateBook1@users.noreply.github.com>
2023-06-28 15:01:44 +0800

    Update 8e9cdddb-b1bb-4984-b926-e8d5725a19da.ts

--- backend/books/8e9cdddb-b1bb-4984-b926-e8d5725a19da.ts
+++ backend/books/8e9cdddb-b1bb-4984-b926-e8d5725a19da.ts
@@ -1,11 +1,11 @@
 export default {
   entity: {
     id: '8e9cdddb-b1bb-4984-b926-e8d5725a19da',
-    name: '宗明兰在省直机关批判右倾翻案风大会上的讲话',
+    name: '宗明兰在省直机关批判右倾翻案风大会上的讲话（1976.12）',
     internal: true,
     official: true,
     type: 'img',
-    author: '',
+    author: '宗明兰，1976.12',
     files:
       'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/8e9cdddb-b1bb-4984-b926-e8d5725a19da/1.jpg,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/8e9cdddb-b1bb-4984-b926-e8d5725a19da/2.jpg,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/8e9cdddb-b1bb-4984-b926-e8d5725a19da/3.jpg,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/8e9cdddb-b1bb-4984-b926-e8d5725a19da/4.jpg,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/8e9cdddb-b1bb-4984-b926-e8d5725a19da/5.jpg,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/8e9cdddb-b1bb-4984-b926-e8d5725a19da/6.jpg,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/8e9cdddb-b1bb-4984-b926-e8d5725a19da/7.jpg,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/8e9cdddb-b1bb-4984-b926-e8d5725a19da/8.jpg,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/8e9cdddb-b1bb-4984-b926-e8d5725a19da/9.jpg,https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/8e9cdddb-b1bb-4984-b926-e8d5725a19da/10.jpg',
   },
