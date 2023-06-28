85a29d98888518129cedd8b1ac6b12cf5ba34852
PirateBook1 <PirateBook1@users.noreply.github.com>
2023-06-28 10:09:51 +0800

    [create-pull-request] automated change

---
+++ backend/books/e6dfb7f2-2632-4705-a13b-da0eb62ea299.ts
@@ -0,0 +1,96 @@
+export default {
+  entity: {
+    id: 'e6dfb7f2-2632-4705-a13b-da0eb62ea299',
+    name: '红旗一九六一年第十三期',
+    internal: false,
+    official: true,
+    type: 'pdf',
+    author: '《红旗》杂志编辑部',
+    files:
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives2/main/e6dfb7f2-2632-4705-a13b-da0eb62ea299.pdf',
+  },
+  parser_option: {
+    page_limits: [],
+    ext: 'pdf',
+    articles: [
+      {
+        title: '在庆祝中国共产党成立四十周年大会上的讲话',
+        authors: ['刘少奇'],
+        page_start: 5,
+        page_end: 15,
+        dates: [{ year: 1961, month: 6, day: 30 }],
+      },
+      {
+        title: '发扬党的优良传统',
+        authors: ['《红旗》杂志编辑部'],
+        page_start: 16,
+        page_end: 20,
+        dates: [{ year: 1961, month: 7 }],
+      },
+      {
+        title: '理论和实践相结合是马克思列宁主义的根本要求',
+        authors: ['范若愚'],
+        page_start: 21,
+        page_end: 26,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 7 }],
+      },
+      {
+        title: '谈胸中有“数”',
+        authors: ['闻师润', '夏金'],
+        page_start: 27,
+        page_end: 31,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 7 }],
+      },
+      {
+        title: '认识规律，运用规律——学习《中国革命战争的战略问题》的笔记',
+        authors: ['卢之超'],
+        page_start: 32,
+        page_end: 38,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 7 }],
+      },
+      {
+        title: '稻田水旱轮栽和“以田养田”',
+        authors: ['丁颖'],
+        page_start: 39,
+        page_end: 46,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 7 }],
+      },
+      {
+        title: '肯尼迪的二三事',
+        authors: ['黄钢'],
+        page_start: 47,
+        page_end: 51,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 7 }],
+      },
+    ],
+    ocr: { content_thresholds: [0, 0.017, 0, 0.2] },
+    ocr_exceptions: {},
+  },
+  parser_id: 'automation',
+  path: '/archives2/e6dfb7f2-2632-4705-a13b-da0eb62ea299.pdf',
+};
