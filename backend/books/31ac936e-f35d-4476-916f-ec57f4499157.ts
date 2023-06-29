8f0b77945cf3adaf73ab0d8719827ba9ad4259bf
PirateBook1 <PirateBook1@users.noreply.github.com>
2023-06-29 14:03:54 +0800

    [create-pull-request] automated change

---
+++ backend/books/31ac936e-f35d-4476-916f-ec57f4499157.ts
@@ -0,0 +1,106 @@
+export default {
+  entity: {
+    id: '31ac936e-f35d-4476-916f-ec57f4499157',
+    name: '红旗一九六一年第十八期',
+    internal: false,
+    official: true,
+    type: 'pdf',
+    author: '《红旗》杂志编辑部',
+    files:
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives2/main/31ac936e-f35d-4476-916f-ec57f4499157.pdf',
+  },
+  parser_option: {
+    page_limits: [],
+    ext: 'pdf',
+    articles: [
+      {
+        title: '进一步加强企业的经济核算',
+        authors: ['许辛学'],
+        page_start: 3,
+        page_end: 6,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 9 }],
+      },
+      {
+        title: '农村人民公社的按劳分配问题',
+        authors: ['章竟'],
+        page_start: 7,
+        page_end: 17,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 9 }],
+      },
+      {
+        title: '关于农村集市贸易',
+        authors: ['管大同'],
+        page_start: 18,
+        page_end: 24,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 9 }],
+      },
+      {
+        title: '历史学科基本训练有关的几个问题',
+        authors: ['白寿彝'],
+        page_start: 25,
+        page_end: 33,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 9 }],
+      },
+      {
+        title: '呕尽心血做学问——谈鲁迅手稿札记',
+        authors: ['王士菁'],
+        page_start: 34,
+        page_end: 39,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 9 }],
+      },
+      {
+        title: '剥开铁托集团“超集团”的画皮',
+        authors: ['施东向'],
+        page_start: 40,
+        page_end: 43,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 9 }],
+      },
+      {
+        title: '汤姆·潘恩的《常识》和肯尼迪的阴谋',
+        authors: ['袁先禄'],
+        page_start: 44,
+        page_end: 47,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 9 }],
+      },
+    ],
+    ocr: { content_thresholds: [0, 0.017, 0, 0.2] },
+    ocr_exceptions: {},
+  },
+  parser_id: 'automation',
+  path: '/archives2/31ac936e-f35d-4476-916f-ec57f4499157.pdf',
+};
