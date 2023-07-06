efade2fa5f60c5bac631fd4e6ad2c01f7becc002
PirateBook1 <PirateBook1@users.noreply.github.com>
2023-07-06 13:35:44 +0800

    [create-pull-request] automated change

---
+++ backend/books/71ed2da7-9f80-44d6-af31-6bd8db0b34a6.ts
@@ -0,0 +1,106 @@
+export default {
+  entity: {
+    id: '71ed2da7-9f80-44d6-af31-6bd8db0b34a6',
+    name: '红旗一九六二年第八、九期',
+    internal: false,
+    official: true,
+    type: 'pdf',
+    author: '《红旗》杂志编辑部',
+    files:
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives2/main/71ed2da7-9f80-44d6-af31-6bd8db0b34a6.pdf',
+  },
+  parser_option: {
+    page_limits: [],
+    ext: 'pdf',
+    articles: [
+      {
+        title: '危机四伏的美国经济',
+        authors: ['孟用潜'],
+        page_start: 3,
+        page_end: 21,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1962, month: 4, day: 25 }],
+      },
+      {
+        title: '鲍威先生的责难和美国国务院的苦恼',
+        authors: ['张振亚'],
+        page_start: 22,
+        page_end: 25,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 4, day: 25 }],
+      },
+      {
+        title: '肯尼迪与产业后备军',
+        authors: ['陈原'],
+        page_start: 25,
+        page_end: 29,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 4, day: 25 }],
+      },
+      {
+        title: '论考茨基主义',
+        authors: ['理夫', '李思温', '王福如'],
+        page_start: 30,
+        page_end: 43,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 4, day: 25 }],
+      },
+      {
+        title: '充分地发挥技术人员的力量',
+        authors: ['石克俭'],
+        page_start: 44,
+        page_end: 47,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 4, day: 25 }],
+      },
+      {
+        title: '数理逻辑与形式逻辑',
+        authors: ['王宪钧'],
+        page_start: 48,
+        page_end: 54,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 4, day: 25 }],
+      },
+      {
+        title: '草原上的拓荒者',
+        authors: ['乌兰巴干'],
+        page_start: 55,
+        page_end: 59,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 4, day: 25 }],
+      },
+    ],
+    ocr: { content_thresholds: [0, 0.017, 0, 0.2] },
+    ocr_exceptions: {},
+  },
+  parser_id: 'automation',
+  path: '/archives2/71ed2da7-9f80-44d6-af31-6bd8db0b34a6.pdf',
+};
