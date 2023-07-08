ddbe4711f144511716e6064e133f10657b79cece
PirateBook1 <PirateBook1@users.noreply.github.com>
2023-07-08 11:47:41 +0800

    [create-pull-request] automated change

---
+++ backend/books/b87fb642-cd7e-488d-93db-a4539420c7a6.ts
@@ -0,0 +1,106 @@
+export default {
+  entity: {
+    id: 'b87fb642-cd7e-488d-93db-a4539420c7a6',
+    name: '红旗一九六二年第十七期',
+    internal: false,
+    official: true,
+    type: 'pdf',
+    author: '《红旗》杂志编辑部',
+    files:
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives2/main/b87fb642-cd7e-488d-93db-a4539420c7a6.pdf',
+  },
+  parser_option: {
+    page_limits: [],
+    ext: 'pdf',
+    articles: [
+      {
+        title: '关于科学人材的选拔和培养问题',
+        authors: ['杨朝俶'],
+        page_start: 3,
+        page_end: 7,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1962, month: 9, day: 1 }],
+      },
+      {
+        title: '有效地提高工业企业劳动生产率',
+        authors: ['海波'],
+        page_start: 8,
+        page_end: 15,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 8, day: 1 }],
+      },
+      {
+        title: '以身作则',
+        authors: ['吴介民'],
+        page_start: 16,
+        page_end: 19,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 8, day: 1 }],
+      },
+      {
+        title: '朝鲜人民建设社会主义的光辉业绩',
+        authors: ['郝德青'],
+        page_start: 20,
+        page_end: 25,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 8, day: 1 }],
+      },
+      {
+        title: '从南斯拉夫经济状况恶化谈起',
+        authors: ['陈茂仪'],
+        page_start: 26,
+        page_end: 33,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 8, day: 1 }],
+      },
+      {
+        title: '所谓“特种战争”的真相',
+        authors: ['陈原'],
+        page_start: 32,
+        page_end: 36,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 8, day: 1 }],
+      },
+      {
+        title: '新中国的考古学',
+        authors: ['夏鼐'],
+        page_start: 37,
+        page_end: 43,
+        ocr: {
+          auto_vsplit: false,
+          vsplit: 0.38,
+          content_thresholds: [0, 0.17, 0, 0.2],
+        },
+        dates: [{ year: 1961, month: 8, day: 1 }],
+      },
+    ],
+    ocr: { content_thresholds: [0, 0.017, 0, 0.2] },
+    ocr_exceptions: {},
+  },
+  parser_id: 'automation',
+  path: '/archives2/b87fb642-cd7e-488d-93db-a4539420c7a6.pdf',
+};
