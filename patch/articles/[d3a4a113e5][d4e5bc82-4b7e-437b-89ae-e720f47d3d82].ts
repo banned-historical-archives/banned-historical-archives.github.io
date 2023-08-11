f4ec05ebdbc75347b25f798b6b7d2d6e55553e07
banned-historical-archives <banned-historical-archives@users.noreply.github.com>
2023-08-11 22:42:32 +0000

    [create-pull-request] automated change

---
+++ patch/articles/[d3a4a113e5][d4e5bc82-4b7e-437b-89ae-e720f47d3d82].ts
@@ -0,0 +1,5 @@
+
+export default [
+// {"version":2,"parts":{"0":{"type":"title"},"1":{"type":"subtitle"},"2":{"type":"authors"},"3":{"diff":"=29\t-1\t+员\t=17\t-1\t+旧\t=26\t-2\t+渣滓\t=10\t-1\t+们\t=37\t-1\t+剧\t=65"}},"comments":{},"description":""}
+  {"version":2,"parts":{"0":{"type":"title"},"1":{"type":"subtitle"},"2":{"type":"authors"},"3":{"diff":"=29\t-1\t+%E5%91%98\t=17\t-1\t+%E6%97%A7\t=26\t-2\t+%E6%B8%A3%E6%BB%93\t=10\t-1\t+%E4%BB%AC\t=37\t-1\t+%E5%89%A7\t=65"}},"comments":{},"description":""},
+];
