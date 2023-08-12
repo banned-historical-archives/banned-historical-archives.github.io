28977d912a60b8b59d6a56a4b35a3613b3be3d02
banned-historical-archives <banned-historical-archives@users.noreply.github.com>
2023-08-12 20:35:02 +0800

    [create-pull-request] automated change

---
+++ backend/books/a043c787-30ac-4642-915a-9a1312d9af4c.ts
@@ -0,0 +1,68 @@
+export default {
+  entity: {
+    id: 'a043c787-30ac-4642-915a-9a1312d9af4c',
+    name: '红旗一九六四年第十五期',
+    internal: false,
+    official: false,
+    type: 'pdf',
+    author: '',
+    files:
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives2/main/a043c787-30ac-4642-915a-9a1312d9af4c.pdf',
+  },
+  parser_option: {
+    page_limits: [],
+    ext: 'pdf',
+    articles: [
+      {
+        title: '中华人民共和国政府声明',
+        authors: ['中华人民共和国政府'],
+        page_start: 3,
+        page_end: 4,
+        dates: [{ year: 1964, month: 8, day: 6 }],
+      },
+      {
+        title: '大力发展和繁荣社会主义戏剧，更好地为社会主义的经济基础服务',
+        authors: ['柯庆施'],
+        page_start: 5,
+        page_end: 24,
+        dates: [{ year: 1964 }],
+      },
+      {
+        title: '为革命的京剧现代化戏欢呼',
+        authors: ['陈其通'],
+        page_start: 25,
+        page_end: 28,
+        dates: [{ year: 1964 }],
+        ocr: { auto_vsplit: false, vsplit: 0.385 },
+      },
+      {
+        title: '坚决将京剧革命进行到底',
+        authors: ['李琪'],
+        page_start: 29,
+        page_end: 33,
+        dates: [{ year: 1964 }],
+        ocr: { auto_vsplit: false, vsplit: 0.385 },
+      },
+      {
+        title: '评周谷城艺术观的哲学基础',
+        authors: ['汝信'],
+        page_start: 34,
+        page_end: 45,
+        dates: [{ year: 1964 }],
+        ocr: { auto_vsplit: false, vsplit: 0.385 },
+      },
+      {
+        title: '美国垄断资本与原子军事工业',
+        authors: ['佟维仁'],
+        page_start: 45,
+        page_end: 51,
+        dates: [{ year: 1964 }],
+        ocr: { auto_vsplit: false, vsplit: 0.385 },
+      },
+    ],
+    ocr: { content_thresholds: [0.07, 0.165, 0, 0.2] },
+    ocr_exceptions: {},
+  },
+  parser_id: 'automation',
+  path: '/archives2/a043c787-30ac-4642-915a-9a1312d9af4c.pdf',
+};
