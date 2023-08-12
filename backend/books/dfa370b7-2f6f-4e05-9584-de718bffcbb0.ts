f8bef78d8b0d9cfeb4b0fb6409051996e74693d2
banned-historical-archives <banned-historical-archives@users.noreply.github.com>
2023-08-12 20:55:59 +0800

    [create-pull-request] automated change

---
+++ backend/books/dfa370b7-2f6f-4e05-9584-de718bffcbb0.ts
@@ -0,0 +1,92 @@
+export default {
+  entity: {
+    id: 'dfa370b7-2f6f-4e05-9584-de718bffcbb0',
+    name: '红旗一九六四年第二十三、二十四期',
+    internal: false,
+    official: false,
+    type: 'pdf',
+    author: '',
+    files:
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives2/main/dfa370b7-2f6f-4e05-9584-de718bffcbb0.pdf',
+  },
+  parser_option: {
+    page_limits: [],
+    ext: 'pdf',
+    articles: [
+      {
+        title: '毛泽东主席关于支持刚果（利）人民反对美国侵略的声明',
+        authors: ['毛泽东'],
+        page_start: 3,
+        page_end: 4,
+        dates: [{ year: 1964 }],
+      },
+      {
+        title: '全世界人民同声讨伐美帝国主义侵略刚果（利）的滔天罪行',
+        authors: ['红旗杂志评论员'],
+        page_start: 5,
+        page_end: 7,
+        dates: [{ year: 1964 }],
+      },
+      {
+        title:
+          '坚决把毛泽东思想学到手——介绍廖初江、丰福生、黄祖示学习毛泽东著作的经验',
+        authors: ['闻师润'],
+        page_start: 8,
+        page_end: 19,
+        dates: [{ year: 1964 }],
+      },
+      {
+        title: '运用一分为二的思想武器，搞好管理教育',
+        authors: ['王玉文'],
+        page_start: 20,
+        page_end: 26,
+        dates: [{ year: 1964, month: 8, day: 21 }],
+        ocr: { auto_vsplit: false, vsplit: 0.385 },
+      },
+      {
+        title: '一分为二是促进生产革命的思想武器——驳“合二而一”谬论',
+        authors: ['王升堂'],
+        page_start: 26,
+        page_end: 28,
+        dates: [{ year: 1964, month: 11, day: 28 }],
+        ocr: { auto_vsplit: false, vsplit: 0.385 },
+      },
+      {
+        title: '“合二而一”就是不要革命',
+        authors: ['张子成'],
+        page_start: 29,
+        page_end: 30,
+        dates: [{ year: 1964 }],
+        ocr: { auto_vsplit: false, vsplit: 0.385 },
+      },
+      {
+        title: '从农村阶级斗争看“合二而一”论的反动性',
+        authors: ['黄学诗'],
+        page_start: 30,
+        page_end: 32,
+        dates: [{ year: 1964 }],
+        ocr: { auto_vsplit: false, vsplit: 0.385 },
+      },
+      {
+        title:
+          '战略上以一当十，战术上以十当一——论毛泽东同志关于人民战争的战略战术思想',
+        authors: ['李作鹏'],
+        page_start: 33,
+        page_end: 49,
+        dates: [{ year: 1964 }],
+      },
+      {
+        title: '约翰逊政府是个什么货色',
+        authors: ['郭济州'],
+        page_start: 50,
+        page_end: 53,
+        dates: [{ year: 1964 }],
+        ocr: { auto_vsplit: false, vsplit: 0.385 },
+      },
+    ],
+    ocr: { content_thresholds: [0.07, 0.165, 0, 0.2] },
+    ocr_exceptions: {},
+  },
+  parser_id: 'automation',
+  path: '/archives2/dfa370b7-2f6f-4e05-9584-de718bffcbb0.pdf',
+};
