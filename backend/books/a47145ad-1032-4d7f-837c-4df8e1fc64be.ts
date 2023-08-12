a88423d94071307693c0524aefb0c655b484385e
banned-historical-archives <banned-historical-archives@users.noreply.github.com>
2023-08-12 18:45:52 +0800

    [create-pull-request] automated change

---
+++ backend/books/a47145ad-1032-4d7f-837c-4df8e1fc64be.ts
@@ -0,0 +1,71 @@
+export default {
+  entity: {
+    id: 'a47145ad-1032-4d7f-837c-4df8e1fc64be',
+    name: '红旗一九六四年第九期',
+    internal: false,
+    official: false,
+    type: 'pdf',
+    author: '',
+    files:
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives2/main/a47145ad-1032-4d7f-837c-4df8e1fc64be.pdf',
+  },
+  parser_option: {
+    page_limits: [],
+    ext: 'pdf',
+    articles: [
+      {
+        title: '中共中央一九六四年五月七日给苏共中央的信',
+        authors: ['中国共产党中央委员会'],
+        page_start: 4,
+        page_end: 9,
+        dates: [{ year: 1964, month: 5, day: 7 }],
+      },
+      {
+        title: '中共中央一九六四年二月二十日给苏共中央的信',
+        authors: ['中国共产党中央委员会'],
+        page_start: 9,
+        page_end: 10,
+        dates: [{ year: 1964, month: 2, day: 20 }],
+      },
+      {
+        title: '中共中央一九六四年二月二十七日给苏共中央的信',
+        authors: ['中国共产党中央委员会'],
+        page_start: 11,
+        page_end: 12,
+        dates: [{ year: 1964, month: 2, day: 27 }],
+      },
+      {
+        title: '中共中央一九六四年二月二十九日给苏共中央的信',
+        authors: ['中国共产党中央委员会'],
+        page_start: 13,
+        page_end: 21,
+        dates: [{ year: 1964, month: 2, day: 29 }],
+      },
+      {
+        title: '苏共中央一九六三年十一月二十九日给中共中央的信',
+        authors: ['赫鲁晓夫'],
+        page_start: 22,
+        page_end: 26,
+        dates: [{ year: 1963, month: 11, day: 29 }],
+      },
+      {
+        title: '苏共中央一九六四年二月二十二日给中共中央的信',
+        authors: ['苏联共产党中央委员会'],
+        page_start: 27,
+        page_end: 30,
+        dates: [{ year: 1964, month: 2, day: 22 }],
+      },
+      {
+        title: '苏共中央一九六四年三月七日给中共中央的信',
+        authors: ['苏联共产党中央委员会'],
+        page_start: 30,
+        page_end: 35,
+        dates: [{ year: 1964, month: 3, day: 7 }],
+      },
+    ],
+    ocr: { content_thresholds: [0.07, 0.165, 0, 0.2] },
+    ocr_exceptions: {},
+  },
+  parser_id: 'automation',
+  path: '/archives2/a47145ad-1032-4d7f-837c-4df8e1fc64be.pdf',
+};
