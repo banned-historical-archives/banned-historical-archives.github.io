b294d5d79c97c43ef58ea983ba21ca700fffb196
banned-historical-archives <banned-historical-archives@users.noreply.github.com>
2023-08-12 17:41:53 +0800

    [create-pull-request] automated change

---
+++ backend/books/f24e1e31-fa48-4979-af79-00b51bd09c48.ts
@@ -0,0 +1,93 @@
+export default {
+  entity: {
+    id: 'f24e1e31-fa48-4979-af79-00b51bd09c48',
+    name: '红旗一九六四年第二、三期',
+    internal: false,
+    official: false,
+    type: 'pdf',
+    author: '',
+    files:
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives2/main/f24e1e31-fa48-4979-af79-00b51bd09c48.pdf',
+  },
+  parser_option: {
+    page_limits: [],
+    ext: 'pdf',
+    articles: [
+      {
+        title:
+          '毛泽东主席对《人民日报》记者发表谈话——中国人民坚决支持巴拿马人民的爱国正义斗争',
+        authors: ['毛泽东'],
+        page_start: 5,
+        page_end: 6,
+        dates: [{ year: 1964, month: 1, day: 12 }],
+      },
+      {
+        title:
+          '毛泽东主席就最近日本反美大示威发表谈话——中国人民支持日本人民伟大的爱国斗争',
+        authors: ['毛泽东'],
+        page_start: 7,
+        page_end: 8,
+        dates: [{ year: 1964, month: 1, day: 27 }],
+      },
+      {
+        title: '苏共领导是当代最大的分裂主义者——七评苏共中央的公开信',
+        authors: ['人民日报编辑部', '红旗杂志编辑部'],
+        page_start: 9,
+        page_end: 35,
+        dates: [{ year: 1964 }],
+      },
+      {
+        title: '永远保持无产阶级的革命性——重读《论共产党员的修养》',
+        authors: ['赵汉'],
+        page_start: 36,
+        page_end: 48,
+        dates: [{ year: 1964 }],
+        ocr: { auto_vsplit: false, vsplit: 0.385 },
+      },
+      {
+        title: '论我国农业的集约经营问题',
+        authors: ['赵天福'],
+        page_start: 49,
+        page_end: 60,
+        dates: [{ year: 1964 }],
+        ocr: { auto_vsplit: false, vsplit: 0.385 },
+      },
+      {
+        title: '提倡现代剧',
+        authors: ['何明'],
+        page_start: 61,
+        page_end: 63,
+        dates: [{ year: 1964 }],
+        ocr: { auto_vsplit: false, vsplit: 0.385 },
+      },
+      {
+        title: '现代剧与艺术趣味',
+        authors: ['王朝闻'],
+        page_start: 64,
+        page_end: 67,
+        dates: [{ year: 1964 }],
+        ocr: { auto_vsplit: false, vsplit: 0.385 },
+      },
+      {
+        title: '把美国侵略者从巴拿马赶出去',
+        authors: ['沙丁'],
+        page_start: 68,
+        page_end: 73,
+        dates: [{ year: 1964 }],
+        ocr: { auto_vsplit: false, vsplit: 0.385 },
+      },
+      {
+        title: '战后美帝国主义与其他帝国主义国家之间的货币斗争',
+        authors: ['项冲'],
+        page_start: 74,
+        page_end: 84,
+        dates: [{ year: 1964 }],
+        ocr: { auto_vsplit: false, vsplit: 0.385 },
+      },
+    ],
+    ocr: { content_thresholds: [0.07, 0.177, 0, 0.2] },
+    ocr_exceptions: {},
+  },
+  parser_id: 'automation',
+  path: '/archives2/f24e1e31-fa48-4979-af79-00b51bd09c48.pdf',
+};
