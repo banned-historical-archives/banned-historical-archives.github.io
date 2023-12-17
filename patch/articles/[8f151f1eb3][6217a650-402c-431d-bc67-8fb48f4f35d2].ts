01a2e1ce311277c9a66e4cff06a52929835eab45
banned-historical-archives <109972625+banned-historical-archives@users.noreply.github.com>
2023-12-17 23:06:46 +0800

    Revert "[BOT][OCR patch]胡乔木同志揭发的邓小平的言论[邓小平言论摘录（续编）][8f151f1eb3][6217a650-402c-431d-bc67-8fb48f4f35d2]"

--- patch/articles/[8f151f1eb3][6217a650-402c-431d-bc67-8fb48f4f35d2].ts
+++
@@ -1,5 +0,0 @@
-
-export default [
-// {"version":2,"parts":{"0":{"delete":true},"1":{"delete":true},"2":{"delete":true},"3":{"delete":true},"4":{"delete":true},"5":{"delete":true},"6":{"delete":true},"7":{"delete":true},"8":{"delete":true},"9":{"delete":true},"10":{"delete":true}},"comments":{},"description":""}
-  {"version":2,"parts":{"0":{"delete":true},"1":{"delete":true},"2":{"delete":true},"3":{"delete":true},"4":{"delete":true},"5":{"delete":true},"6":{"delete":true},"7":{"delete":true},"8":{"delete":true},"9":{"delete":true},"10":{"delete":true}},"comments":{},"description":""},
-];
