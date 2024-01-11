3639fdb55dce528573d992bbb51159c5ba673e36
Your Name <you@example.com>
2024-01-11 08:58:34 +0800

    wip

---
+++ backend/gitworkflow-get-safe-title.ts
@@ -0,0 +1,18 @@
+import { exec, execSync } from 'node:child_process';
+import { join, basename, dirname, extname } from 'node:path/posix';
+import { isAbsolute } from 'node:path';
+import https from 'https';
+import { Transform } from 'stream';
+import fs from 'node:fs';
+import { ParserOptionV2 } from '../types';
+import JSON5 from 'json5';
+
+export async function start() {
+  const body = (process.env as any).TITLE.trim();
+  const res = (body.split(']')[1] || '').replace(/[\\\'\"\.\-\(\)]/g, '').split('\r')[0].split('\n')[0];
+  console.log(res || 'unknown');
+}
+
+if (!(process.env as any).TEST) {
+  start();
+}
