d933656199d9fd03cbed559c3bd5c039c4ea35d1
Your Name <you@example.com>
2024-01-14 11:54:50 +0800

    fix

--- components/ImageTags.tsx
+++ components/ImageTags.tsx
@@ -3,9 +3,9 @@ import Typography from '@mui/material/Typography';
 import { ReactElement, useState, useEffect, useMemo } from 'react';
 import Popover from '@mui/material/Popover';
 import { ArticleCategory, ArticleType, TagType } from '../types';
-import { ImageTag } from '../backend/entities';
+import { Tag } from '../types';
 
-export default function ImageTags({ tags }: { tags: ImageTag[] }) {
+export default function ImageTags({ tags }: { tags: Tag[] }) {
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
   const [label, setLabel] = useState('');
 
@@ -34,7 +34,7 @@ export default function ImageTags({ tags }: { tags: ImageTag[] }) {
 
       {tags.map((tag) => (
         <Chip
-          key={tag.id}
+          key={tag.type + '##' + tag.name}
           onMouseEnter={(event) => {
             setLabel(tag.type);
             setAnchorEl(event.currentTarget);
