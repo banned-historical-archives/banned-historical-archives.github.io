36517dad5c3baa0b57dcaa4b77b9c398e4fc6fa3
Your Name <you@example.com>
2024-02-08 00:20:02 +0800

    fix:build

--- backend/gitworkflow-get-archive-id.ts
+++ backend/gitworkflow-get-archive-id.ts
@@ -4,7 +4,7 @@ import { isAbsolute } from 'node:path';
 import https from 'https';
 import { Transform } from 'stream';
 import fs, { readFileSync } from 'node:fs';
-import { ArticleIndexes, BookIndexes, AutomationEntryOption } from '../types';
+import { ArticleIndexes, BookIndexes, AutomatedEntryOption } from '../types';
 import JSON5 from 'json5';
 
 const body = (process.env as any).BODY.trim();
@@ -31,10 +31,7 @@ export async function start() {
   try {
     const body1: string = body.substring(0, body.lastIndexOf('```'));
     const body2: string = body1.substring(body1.lastIndexOf('```') + 3);
-    const config = JSON5.parse(body2) as AutomationEntryOption & {
-      id: string;
-      source_name: string;
-    };
+    const config = JSON5.parse(body2) as AutomatedEntryOption;
     config.archive_id = config.archive_id == undefined ? 1 : config.archive_id;
     if (config.archive_id >= 0) {
       console.log(config.archive_id);
