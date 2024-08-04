fccf87d4fb1e00cd442cdcf72e0d1a07b958f45d
Your Name <you@example.com>
2024-08-04 21:25:25 +0800

    migration

--- migration/fix_patch/index.js
+++ migration/fix_patch/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs-extra');
+const {exec} = require('child_process')
 const {get_article_id, new_get_article_id} = require('../fixmd5/utils');
 const {join} = require('path')
 const JSON5 = require('json5')
@@ -41,7 +42,7 @@ const cfgs = {};
             try {
             cfg.parser_option.articles.forEach(x=> {
                 ids[get_article_id(x)] = new_get_article_id(x);
-                cfgs[get_article_id(x)]=i;
+                cfgs[get_article_id(x)]=[i,x, cfg];
             });
         }catch(e){
             console.log(cfg)
@@ -55,6 +56,7 @@ const cfgs = {};
 
   v1.forEach(i => {
     const old_aid = i.split('[')[1].split(']')[0];
+    const book_id = i.split(']')[1].split('[')[1];
     if (!ids[old_aid]) {
   // 忽略2个patch
   // maoistlibrary, wanghongwen
@@ -67,14 +69,55 @@ const cfgs = {};
         return;
     }
 
-    console.log(old_aid, ids[old_aid], cfgs[old_aid]);
+    // 注意：一部分文稿使用cnocr2.2,一部分使用cnocr2.1??。其ocr结果有差异，会影响ocr patch的运行。
+    // 目前没有记录使用的cnocr版本，需要重新运行ocr才能判断版本
+
+    // 因为文章页码可能重叠，ocr cache只能选择一种模型，运行下面的代码后需要特殊处理重叠的文章
+
+    // console.log(old_aid, ids[old_aid], cfgs[old_aid]);
     const json = JSON5.parse(fs.readFileSync('./v1/' + i ).toString().replace('export default', '').trim().replace(/;$/, ''));
     let content = JSON.stringify(json);
     content = content.substring(1);
     content = content.substring(0, content.length - 1);
-    fs.writeFileSync(`../../ocr_patch/archives${cfgs[old_aid]}/[${ids[old_aid]}]${i.split(']')[1]}].ts`,
+
+    // if (ids[old_aid] != 'bada7c4d88') return;
+
+    const [archives_id,cfg,book_cfg] = cfgs[old_aid];
+
+    // 先删除旧的ocr结果
+    for (let a = cfg.page_start; a <= cfg.page_end; ++a) {
+      try {
+      fs.unlinkSync(`../../ocr_cache/archives${archives_id}/${book_id}/${a}.json`);
+      }catch(e) {}
+    }
+
+    console.log(archives_id)
+    // 更新config, 设置side len 为1496
+    const target = book_cfg.parser_option.articles.find(i => new_get_article_id(i) == ids[old_aid]);
+    target.ocr = {
+      ...target.ocr,
+      det_limit_side_len: 1496,
+      det_db_box_thresh: 0.3,
+      rec_model_dir: './paddle/ch_ppocr_mobile_v2.0_rec_infer',
+      det_model_dir: './paddle/ch_PP-OCRv3_det_infer',
+      drop_score: 0.3
+    };
+    fs.writeFileSync(`../../config/archives${archives_id}/${book_id}.ts`, 'export default ' + JSON.stringify(book_cfg, null, 2) + ';');
+
+    // 更新patch
+
+    fs.writeFileSync(`../../ocr_patch/archives${archives_id}/[${ids[old_aid]}][${book_id}].ts`,
         'export default [\n' + content +',\n];'
     );
-    //console.log(json);
+
   });
+
+  // 手动运行
+  // 重新运行ocr
+  // const {stdout, stderr} = exec(`npm run build_ocr_cache -- ../banned-historical-archives.github.io/config/archives2 ../banned-historical-archives.github.io/raw/archives2 ../banned-historical-archives.github.io/ocr_cache/archives2`, {cwd: '../../ocr_helper'})
+
+  // 更新parsed
+  // const {stdout, stderr} = exec(`npm run build_parsed -- ../banned-historical-archives.github.io/config/archives2 ../banned-historical-archives.github.io/ocr_cache/archives2 ../banned-historical-archives.github.io/ocr_patch/archives2 ../banned-historical-archives.github.io/parsed/archives2 ../banned-historical-archives.github.io/raw/archives2`, {cwd: '../../ocr_helper'})
+  //console.log(json);
+ // console.log(stdout)
 })();
