08a8bc621f045ad7a5f7c28419465b68879933a3
banned-historical-archives <109972625+banned-historical-archives@users.noreply.github.com>
2024-08-08 08:24:12 +0800

    Update [id].tsx

--- pages/articles/[id].tsx
+++ pages/articles/[id].tsx
@@ -443,13 +443,13 @@ export default function ArticleViewer({
               onClick={() =>
                 window.open(
                   `https://github.com/banned-historical-archives/banned-historical-archives.github.io/issues?q=+${encodeURIComponent(
-                    `is:pr "${articleId}" "${i.id}"`,
+                    `is:issue "${articleId}" "${i.id}"`,
                   )}+`,
                   '_blank',
                 )
               }
               src={`https://img.shields.io/github/issues-search/banned-historical-archives/banned-historical-archives.github.io?style=for-the-badge&color=%23cc0000&label=%E6%A0%A1%E5%AF%B9%E8%AE%B0%E5%BD%95&query=${encodeURIComponent(
-                `is:pr "${articleId}" "${i.id}"`,
+                `is:issue "${articleId}" "${i.id}"`,
               )}`}
             />
           ))}
