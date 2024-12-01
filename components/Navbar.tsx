889ca8b5a02cd2197a7435a38ebcab10d7ae2b2c
Your Name <you@example.com>
2024-12-01 09:28:55 +0800

    fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -172,7 +172,7 @@ export default function Navbar() {
                   if (e.key === 'Enter') {
                     if (process?.env?.LOCAL_SEARCH_ENGINE) {
                       window.open(
-                        `${location.protocol}//${location.hostname}/search?keyword=` +
+                        `${location.protocol}//${location.host}/search?keyword=` +
                           encodeURIComponent(inputValue),
                         '_blank',
                       );
