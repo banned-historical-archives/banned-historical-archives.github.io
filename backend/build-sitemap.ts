472064d9c591da762d87db4bd118ec2119ef1397
Your Name <you@example.com>
2024-12-16 21:56:38 +0800

    fix: build sitemap

---
+++ backend/build-sitemap.ts
@@ -0,0 +1,56 @@
+import { readFileSync } from 'fs';
+import {writeFileSync} from'fs-extra';
+import { join } from 'path';
+import { ArticleIndexes } from '../types';
+
+const article_indexes = JSON.parse(
+  readFileSync(join(process.cwd(), 'article_indexes.json')).toString(),
+) as ArticleIndexes;
+
+const ids = Object.keys(article_indexes);
+
+const dir = 'out';
+writeFileSync(join(dir, `robot.txt`), `# *
+User-agent: *
+Allow: /
+
+# Host
+Host: https://banned-historical-archives.github.io
+
+# Sitemaps
+Sitemap: https://banned-historical-archives.github.io/sitemap.xml`);
+
+writeFileSync(join(dir, `sitemap.xml`), `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<sitemap><loc>https://banned-historical-archives.github.io/sitemap-0.xml</loc></sitemap>
+</sitemapindex>`);
+
+const chunk_size = 4000;
+const n = Math.ceil(ids.length / chunk_size);
+const now = (new Date()).toISOString();
+for (let i = 0; i < n; i++) {
+  const x = ids.slice(i * chunk_size, (i + 1) * chunk_size);
+  writeFileSync(
+    join(dir, `sitemap-${i}.xml`),
+    `
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+${
+  i == 0
+    ? `<url><loc>https://banned-historical-archives.github.io</loc><lastmod>${now}</lastmod><changefreq>daily</changefreq><priority>0.7</priority></url>
+<url><loc>https://banned-historical-archives.github.io/articles</loc><lastmod>${now}</lastmod><changefreq>daily</changefreq><priority>0.7</priority></url>
+<url><loc>https://banned-historical-archives.github.io/gallery</loc><lastmod>${now}</lastmod><changefreq>daily</changefreq><priority>0.7</priority></url>
+<url><loc>https://banned-historical-archives.github.io/music</loc><lastmod>${now}</lastmod><changefreq>daily</changefreq><priority>0.7</priority></url>
+<url><loc>https://banned-historical-archives.github.io/search</loc><lastmod>${now}</lastmod><changefreq>daily</changefreq><priority>0.7</priority></url>
+<url><loc>https://banned-historical-archives.github.io/articles/1001069083</loc><lastmod>${now}</lastmod><changefreq>daily</changefreq><priority>0.7</priority></url>`
+    : ''
+}
+${x
+  .map(
+    (t) =>
+      `<url><loc>https://banned-historical-archives.github.io/article?id=${t}</loc><lastmod>${now}</lastmod><changefreq>daily</changefreq><priority>0.7</priority></url>`,
+  )
+  .join('\n')}
+</urlset>`,
+  );
+}
